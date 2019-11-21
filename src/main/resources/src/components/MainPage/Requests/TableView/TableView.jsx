19c0150c34eb4bcd93b4597d918bd2c367908978
iteratorOne <ilyasudakov@inbox.ru>
2019-11-21 13:43:42 +0300

    add product table, fix multiproducts lines in reqs

--- src/main/resources/src/components/MainPage/Requests/TableView/TableView.jsx
+++ src/main/resources/src/components/MainPage/Requests/TableView/TableView.jsx
@@ -54,41 +54,68 @@ const TableView = (props) => {
     }
 
     return (
-        <div className="tableview">
-            <div className="tableview__row tableview__row--header">
-                <div className="tableview__col" data-label="ID">
+        <div className="tableview_requests">
+            <div className="tableview_requests__row tableview_requests__row--header">
+                <div className="tableview_requests__col">
                     <span>ID</span>
-                    <img name="id" className="tableview__img" onClick={changeSortOrder} src={sortIcon} />
+                    <img name="id" className="tableview_requests__img" onClick={changeSortOrder} src={sortIcon} />
                 </div>
-                <div className="tableview__col" data-label="Дата">
+                <div className="tableview_requests__col">
                     <span>Дата</span>
-                    <img name="date" className="tableview__img" onClick={changeSortOrder} src={sortIcon} />
+                    <img name="date" className="tableview_requests__img" onClick={changeSortOrder} src={sortIcon} />
                 </div>
-                <div className="tableview__col" data-label="Продукция">Продукция</div>
-                <div className="tableview__col" data-label="Количество">Количество</div>
-                <div className="tableview__col" data-label="Кодовое слово">Кодовое слово</div>
-                <div className="tableview__col" data-label="Ответственный">Ответственный</div>
-                <div className="tableview__col" data-label="Статус">Статус</div>
-                <div className="tableview__col" data-label="Действия">Действия</div>
+                <div className="tableview_requests__col">Продукция</div>
+                <div className="tableview_requests__col">Количество</div>
+                <div className="tableview_requests__col">Кодовое слово</div>
+                <div className="tableview_requests__col">Ответственный</div>
+                <div className="tableview_requests__col">Статус</div>
+                <div className="tableview_requests__col">Действия</div>
             </div>
             {sortRequests(props.data).map((request, request_id) => (
-                <div key={request_id} className={"tableview__row " +
-                    (request.status === "Не готово" && "tableview__row--status_not_ready" ||
-                        request.status === "В процессе" && "tableview__row--status_in_progress" ||
-                        request.status === "Готово к отгрузке" && "tableview__row--status_ready" ||
-                        request.status === "Отгружено" && "tableview__row--status_shipped")
+                <div key={request_id} className={"tableview_requests__row " +
+                    (request.status === "Не готово" && "tableview_requests__row--status_not_ready" ||
+                        request.status === "В процессе" && "tableview_requests__row--status_in_progress" ||
+                        request.status === "Готово к отгрузке" && "tableview_requests__row--status_ready" ||
+                        request.status === "Отгружено" && "tableview_requests__row--status_shipped")
                 }>
-                    <div className="tableview__col" data-label="ID">{request.id}</div>
-                    <div className="tableview__col" data-label="Дата">{formatDateString(request.date)}</div>
-                    <div className="tableview__col" data-label="Продукция">{request.products}</div>
-                    <div className="tableview__col" data-label="Количество">{request.quantity}</div>
-                    <div className="tableview__col" data-label="Кодовое слово">{request.codeWord}</div>
-                    <div className="tableview__col" data-label="Ответственный">{request.responsible}</div>
-                    <div className="tableview__col" data-label="Статус">{request.status}</div>
-                    <div className="tableview__col" data-label="Действия" className="tableview__actions">
-                        {/* <div data-id={request.id} className="tableview__action" >Просмотр</div> */}
-                        <div data-id={request.id} className="tableview__action" >Редактировать</div>
-                        {/* <div data-id={request.id} className="tableview__action" onClick={props.deleteItem}>Удалить</div> */}
+                    <div className="tableview_requests__col">{request.id}</div>
+                    <div className="tableview_requests__col">{formatDateString(request.date)}</div>
+                    {/* Корректный вывод в строку без ограничения (в разработке) */}
+                    <div className="tableview_requests__col">
+                        <div className="tableview_requests__sub_row" style={{height: `calc(${100/3}%)`}}>
+                            <div className="tableview_requests__sub_col">{request.products}</div>
+                            <div className="tableview_requests__sub_col">{request.quantity}</div>
+                        </div>
+                        <div className="tableview_requests__sub_row" style={{height: `calc(${100/3}%)`}}>
+                            <div className="tableview_requests__sub_col">{request.products}</div>
+                            <div className="tableview_requests__sub_col">{request.quantity}</div>
+                        </div>
+                        <div className="tableview_requests__sub_row" style={{height: `calc(${100/3}%)`}}>
+                            <div className="tableview_requests__sub_col">{request.products}</div>
+                            <div className="tableview_requests__sub_col">{request.quantity}</div>
+                        </div>
+                    </div>
+
+                    {/* Вывод продуктов и кол-ва как строки */}
+                    {/* <div className="tableview_requests__col">{request.products}</div>
+                    <div className="tableview_requests__col">{request.quantity}</div> */}
+
+                    {/* Корректный вывод но с ограничением по количеству символов в строке */}
+                    {/* <div className="tableview_requests__col">
+                        <div className="tableview_requests__subrow" style={{height: `${100/2}%`}}><div className="tableview_requests__subtext">{request.products}</div></div>
+                        <div className="tableview_requests__subrow" style={{height: `${100/2}%`}}><div className="tableview_requests__subtext">{request.products}</div></div>
+                    </div>
+                    <div className="tableview_requests__col">
+                        <div className="tableview_requests__subrow" style={{height: `${100/2}%`}}><div className="tableview_requests__subtext">{request.quantity}</div></div>
+                        <div className="tableview_requests__subrow" style={{height: `${100/2}%`}}><div className="tableview_requests__subtext">{request.quantity}</div></div>
+                    </div> */}
+                    <div className="tableview_requests__col">{request.codeWord}</div>
+                    <div className="tableview_requests__col">{request.responsible}</div>
+                    <div className="tableview_requests__col">{request.status}</div>
+                    <div className="tableview_requests__actions">
+                        {/* <div data-id={request.id} className="tableview_requests__action" >Просмотр</div> */}
+                        <div data-id={request.id} className="tableview_requests__action" >Редактировать</div>
+                        {/* <div data-id={request.id} className="tableview_requests__action" onClick={props.deleteItem}>Удалить</div> */}
                     </div>
                 </div>
             ))}
