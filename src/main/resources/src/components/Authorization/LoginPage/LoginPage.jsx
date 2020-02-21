89260606751cd0cb90dbe2877de3b24de8052f7e
iteratorOne <ilyasudakov@inbox.ru>
2020-02-21 21:28:08 +0300

    add password and login imgs to LoginPage

--- src/main/resources/src/components/Authorization/LoginPage/LoginPage.jsx
+++ src/main/resources/src/components/Authorization/LoginPage/LoginPage.jsx
@@ -3,6 +3,10 @@ import { Link, Redirect } from 'react-router-dom';
 import './LoginPage.scss';
 import { login, refreshToken } from '../../../utils/RequestsAPI/Authorization.jsx';
 import profileSVG from '../../../../../../../assets/header/profile1.svg';
+import companyLogo from '../../../../../../../assets/priceList/osfix_logo.png';
+import PasswordIcon from '../../../../../../../assets/loginPage/password.png';
+import eyeIcon from '../../../../../../../assets/loginPage/eye.png';
+import eyeClosedIcon from '../../../../../../../assets/loginPage/eye_closed.png';
 import ErrorMessage from '../../../utils/Form/ErrorMessage/ErrorMessage.jsx';
 import exitSVG from '../../../../../../../assets/header/exit.svg';
 
@@ -10,6 +14,7 @@ const LoginPage = (props) => {
     const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
     const [showError, setShowError] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
@@ -49,8 +54,9 @@ const LoginPage = (props) => {
             {!props.isAuthorized ? (
                 <React.Fragment>
                     <div className="authorization__title">
-                        <img className="authorization__img" src={profileSVG} alt="" />
-                        <span>Авторизация</span>
+                        {/* <img className="authorization__img" src={profileSVG} alt="" /> */}
+                        <img className="authorization__img authorization__img--logo" src={companyLogo} alt="" />
+                        {/* <span>Авторизация</span> */}
                     </div>
                     <div className="authorization__panel">
                         <ErrorMessage
@@ -62,13 +68,17 @@ const LoginPage = (props) => {
                             Email
                         </div> */}
                         <div className="authorization__field_input">
+                            <img className="authorization__img" src={profileSVG} alt="" />
                             <input type="text" onChange={e => setUserName(e.target.value)} placeholder="Введите логин..." defaultValue="" />
                         </div>
                         {/* <div className="authorization__field_name">
                             Пароль
                         </div> */}
                         <div className="authorization__field_input">
-                            <input type="password" onChange={e => setPassword(e.target.value)} placeholder="Введите пароль..." />
+                            <img className="authorization__img authorization__img--eye" onClick={() => { setShowPassword(!showPassword) }} src={eyeIcon} alt="" />
+                            <div className={showPassword ? "authorization__line" : "authorization__line authorization__line--hidden"} onClick={() => { setShowPassword(!showPassword) }} ></div>
+                            <img className="authorization__img authorization__img--password" src={PasswordIcon} alt="" />
+                            <input type={showPassword ? "text" : "password"} onChange={e => setPassword(e.target.value)} placeholder="Введите пароль..." />
                         </div>
                         <button className="authorization__submit" onClick={handleLogin} >
                             <span>Войти</span>
@@ -79,8 +89,9 @@ const LoginPage = (props) => {
             ) : (
                     <React.Fragment>
                         <div className="authorization__title">
-                            <img className="authorization__img" src={profileSVG} alt="" />
-                            <span>Выход из аккаунта</span>
+                            {/* <img className="authorization__img" src={profileSVG} alt="" /> */}
+                            <img className="authorization__img authorization__img--logo" src={companyLogo} alt="" />
+                            {/* <span>Выход из аккаунта</span> */}
                         </div>
                         <div className="authorization__panel">
                             <button className="authorization__submit" onClick={handleSignOut}>
