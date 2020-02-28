51c243320e51ed1599d9fe752f88437150a043dd
iteratorOne <ilyasudakov@inbox.ru>
2020-02-28 22:27:47 +0300

    fix InputProducts show in SelectWork

--- src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/SelectWork/SelectWork.jsx
+++ src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/SelectWork/SelectWork.jsx
@@ -163,7 +163,7 @@ const SelectWork = (props) => {
                             />
                             {/* Вставить InputProducts, только вместо фасовки сделать 
                                 единицу измерения(или просто кол-во оставить) */}
-                            {selected[index].workType === 'Продукция'
+                            {(selected[index].workType === 'Продукция' || selected[index].workType === undefined)
                                 ?
                                 <InputProducts
                                     inputName="Продукция"
