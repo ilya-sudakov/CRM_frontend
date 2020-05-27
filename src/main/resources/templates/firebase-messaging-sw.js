937b1110a8751ff4bdeec6614b5636dd46c1707d
ilyasudakov <ilyasudakov@inbox.ru>
2020-05-27 23:12:31 +0300

    add browser support check for firebase

--- src/main/resources/templates/firebase-messaging-sw.js
+++ src/main/resources/templates/firebase-messaging-sw.js
@@ -6,40 +6,23 @@ firebase.initializeApp({
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 })
 const messaging = firebase.messaging()
-// messaging.setBackgroundMessageHandler(function (payload) {
-//   const promiseChain = clients
-//     .matchAll({
-//       type: 'window',
-//       includeUncontrolled: true,
-//     })
-//     .then((windowClients) => {
-//       for (let i = 0; i < windowClients.length; i++) {
-//         const windowClient = windowClients[i]
-//         windowClient.postMessage(payload)
-//       }
-//     })
-//     .then(() => {
-//       return registration.showNotification('my notification title')
-//     })
-//   return promiseChain
-// })
-// self.addEventListener('notificationclick', function (event) {
-//   // do what you want
-//   // ...
-// })
 
-messaging.setBackgroundMessageHandler(function (payload) {
-  console.log('Handling background message ', payload)
+if (firebase.messaging.isSupported()) {
+  messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('Handling background message ', payload)
 
-  return self.registration.showNotification(payload.data.title, {
-    body: payload.data.body,
-    icon: payload.data.icon,
-    tag: payload.data.tag,
-    data: payload.data.link,
+    return self.registration.showNotification(payload.data.title, {
+      body: payload.data.body,
+      icon: payload.data.icon,
+      tag: payload.data.tag,
+      data: payload.data.link,
+    })
   })
-})
 
-self.addEventListener('notificationclick', function (event) {
-  event.notification.close()
-  event.waitUntil(self.clients.openWindow(event.notification.data))
-})
+  self.addEventListener('notificationclick', function (event) {
+    event.notification.close()
+    event.waitUntil(self.clients.openWindow(event.notification.data))
+  })
+} else {
+  console.log('browser not supported for firebase')
+}
