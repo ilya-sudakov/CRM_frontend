d1984587589ec5a0b6d6c68a19cd8b235a0b325b
ilyasudakov <ilyasudakov@inbox.ru>
2020-06-22 22:18:13 +0300

    update list style in WorkManagementPage

--- src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/WorkManagementPage/TableView/TableView.jsx
+++ src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/WorkManagementPage/TableView/TableView.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './TableView.scss'
 import {
   numberToString,
@@ -7,9 +7,27 @@ import {
 import TableDataLoading from '../../../../../../utils/TableView/TableDataLoading/TableDataLoading.jsx'
 
 const TableView = (props) => {
-  const onClickHandle = (employeeId, date, value) => {
+  useEffect(() => {}, [props.data])
+
+  const onClickHandle = (employee, date, value) => {
     let newData = props.data
-    console.log(employeeId, date, value)
+    // console.log(employee, date, value)
+    // newData = Object.assign({
+    //   ...newData,
+    //   [employee.workshop]: {
+    //     ...newData[employee.workshop],
+    //     [date]: {
+    //       ...newData[employee.workshop][date],
+    //       [employee.id]: {
+    //         ...newData[employee.workshop][date][employee.id],
+    //         isOpen: !newData[employee.workshop][date][employee.id].isOpen,
+    //       },
+    //     },
+    //   },
+    // })
+    // console.log(newData, newData)
+    newData[date][employee.id].isOpen = !value
+    props.onChange(newData)
   }
 
   return (
@@ -18,100 +36,111 @@ const TableView = (props) => {
         <TableDataLoading className="work-management-page__row" />
       )}
       {Object.entries(props.data).map((date) => {
+        let index = 0
         return (
           <>
             <div className="work-management-page__table-date">
               {formatDateString(date[0])}
             </div>
-            {Object.values(date[1]).map((item) => {
-              console.log(item)
-              return (
-                <div
-                  className={
-                    item.isOpen
-                      ? 'work-management-page__row'
-                      : 'work-management-page__row work-management-page__row--hidden'
-                  }
-                  onClick={() => {
-                    onClickHandle(item.employee.id, date[0], true)
-                  }}
-                >
-                  <div className="work-management-page__item work-management-page__item--header">
-                    <span>{item.employee.position + ' '}</span>
-                    <span>
-                      {' ' +
-                        item.employee.lastName +
-                        ' ' +
-                        item.employee.name +
-                        ' ' +
-                        item.employee.middleName}
-                    </span>
-                    <span>
-                      {Math.floor(
-                        item.works.reduce((sum, cur) => {
-                          if (cur.hours !== undefined) {
-                            return sum + cur.hours
-                          } else {
-                            return sum
-                          }
-                        }, 0) * 100,
-                      ) /
-                        100 +
-                        ' ' +
-                        numberToString(
-                          Math.floor(
-                            item.works.reduce((sum, cur) => {
-                              if (cur.hours !== undefined) {
-                                return sum + cur.hours
-                              } else {
-                                return sum
-                              }
-                            }, 0) * 100,
-                          ) / 100,
-                          ['час', 'часа', 'часов'],
-                        )}
-                    </span>
+            {Object.values(date[1])
+              .sort((a, b) => {
+                if (a.employee.lastName < b.employee.lastName) {
+                  return -1
+                }
+                if (a.employee.lastName > b.employee.lastName) {
+                  return 1
+                }
+                return 0
+              })
+              .map((item) => {
+                return (
+                  <div
+                    // index={index++}
+                    className={
+                      item.isOpen
+                        ? 'work-management-page__row'
+                        : 'work-management-page__row work-management-page__row--hidden'
+                    }
+                    onClick={() => {
+                      onClickHandle(item.employee, date[0], item.isOpen)
+                    }}
+                  >
+                    <div className="work-management-page__item work-management-page__item--header">
+                      <span>{item.employee.position + ' '}</span>
+                      <span>
+                        {' ' +
+                          item.employee.lastName +
+                          ' ' +
+                          item.employee.name +
+                          ' ' +
+                          item.employee.middleName}
+                      </span>
+                      <span>
+                        {Math.floor(
+                          item.works.reduce((sum, cur) => {
+                            if (cur.hours !== undefined) {
+                              return sum + cur.hours
+                            } else {
+                              return sum
+                            }
+                          }, 0) * 100,
+                        ) /
+                          100 +
+                          ' ' +
+                          numberToString(
+                            Math.floor(
+                              item.works.reduce((sum, cur) => {
+                                if (cur.hours !== undefined) {
+                                  return sum + cur.hours
+                                } else {
+                                  return sum
+                                }
+                              }, 0) * 100,
+                            ) / 100,
+                            ['час', 'часа', 'часов'],
+                          )}
+                      </span>
+                    </div>
+                    {item.works.map((workItem, index) => {
+                      return (
+                        <div className="work-management-page__item">
+                          <div>
+                            {workItem.workList.work +
+                              ' - ' +
+                              workItem.hours +
+                              ' ' +
+                              numberToString(
+                                Number.parseInt(
+                                  Math.floor(workItem.hours * 100) / 100,
+                                ),
+                                ['час', 'часа', 'часов'],
+                              )}
+                          </div>
+                          <div className="work-management-page__item work-management-page__item--products">
+                            {workItem.workControlProduct.map((product) => {
+                              return (
+                                <div>
+                                  {product.product.name +
+                                    ' (' +
+                                    product.quantity +
+                                    ' шт.)'}
+                                </div>
+                              )
+                            })}
+                            {workItem.partsWorks.map((draft) => {
+                              return (
+                                <div>
+                                  {draft.name + ' (' + draft.quantity + ' шт.)'}
+                                </div>
+                              )
+                            })}
+                          </div>
+                        </div>
+                      )
+                    })}
                   </div>
-                  {item.works.map((workItem, index) => {
-                    return (
-                      <div className="work-management-page__item">
-                        <div>
-                          {workItem.workList.work +
-                            ' - ' +
-                            workItem.hours +
-                            ' ' +
-                            numberToString(
-                              Number.parseInt(
-                                Math.floor(workItem.hours * 100) / 100,
-                              ),
-                              ['час', 'часа', 'часов'],
-                            )}
-                        </div>
-                        <div className="work-management-page__item work-management-page__item--products">
-                          {workItem.workControlProduct.map((product) => {
-                            return (
-                              <div>
-                                {product.product.name +
-                                  ' (' +
-                                  product.quantity +
-                                  ' шт.)'}
-                              </div>
-                            )
-                          })}
-                          {workItem.partsWorks.map((draft) => {
-                            return (
-                              <div>
-                                {draft.name + ' (' + draft.quantity + ' шт.)'}
-                              </div>
-                            )
-                          })}
-                        </div>
-                      </div>
-                    )
-                  })}
-                </div>
-              )
-            })}
+                )
+              })}
           </>
         )
       })}
