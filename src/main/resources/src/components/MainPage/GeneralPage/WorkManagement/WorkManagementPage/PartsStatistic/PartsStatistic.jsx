c47640ef5f17837d93bfb26aad446c83a3fbcf6a
ilyasudakov <ilyasudakov@inbox.ru>
2020-06-24 19:25:15 +0300

    update PartsStatistic header

--- src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/WorkManagementPage/PartsStatistic/PartsStatistic.jsx
+++ src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/WorkManagementPage/PartsStatistic/PartsStatistic.jsx
@@ -249,6 +249,7 @@ const PartsStatistic = (props) => {
               !isVisible &&
               '0 1px 1px rgba(0, 0, 0, 0.05), 0 0px 5px rgba(0, 0, 0, 0.1)',
             // backgroundColor: '#ffffff',
+            maxWidth: isVisible && 'calc(100% - 30px)',
           }}
         >
           <div
