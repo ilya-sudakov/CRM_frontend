142d30c9cded2e08a0157ef314e26da576aa3e54
ilyasudakov <ilyasudakov@inbox.ru>
2020-07-28 21:00:14 +0300

    remove copyAll button

--- src/main/resources/src/components/MainPage/Dispatcher/Rigging/Stamp/Stamp.jsx
+++ src/main/resources/src/components/MainPage/Dispatcher/Rigging/Stamp/Stamp.jsx
@@ -4,20 +4,12 @@ import './Stamp.scss'
 import '../../../../../utils/MainWindow/MainWindow.scss'
 import TableView from '../RiggingComponents/TableView/TableView.jsx'
 import {
-  getStamp,
   getStampById,
   deletePartsFromStamp,
   deleteStamp,
-  addStamp,
-  addPartsToStamp,
   getStampsByStatus,
-  editStamp,
 } from '../../../../../utils/RequestsAPI/Rigging/Stamp.jsx'
 import FloatingPlus from '../../../../../utils/MainWindow/FloatingPlus/FloatingPlus.jsx'
-import { getPressForm } from '../../../../../utils/RequestsAPI/Rigging/PressForm.jsx'
-import { getMachine } from '../../../../../utils/RequestsAPI/Rigging/Machine.jsx'
-import { getParts } from '../../../../../utils/RequestsAPI/Parts.jsx'
-import Button from '../../../../../utils/Form/Button/Button.jsx'
 
 const Stamp = (props) => {
   const [stamps, setStamps] = useState([])
@@ -38,7 +30,6 @@ const Stamp = (props) => {
   const loadStamps = (signal) => {
     setIsLoading(true)
     getStampsByStatus('stamp', signal)
-      // getStamp(signal)
       .then((res) => res.json())
       .then((res) => {
         setIsLoaded(true)
@@ -62,135 +53,6 @@ const Stamp = (props) => {
       )
       .then(() => deleteStamp(id))
       .then(() => loadStamps())
-  }
-
-  const handleCopyDataToStamp = () => {
-    setIsLoading(true)
-    return getStamp()
-      .then((res) => res.json())
-      .then((res) =>
-        Promise.all(
-          res.map((stamp) =>
-            editStamp({ ...stamp, status: 'stamp' }, stamp.id),
-          ),
-        ),
-      )
-      .then(() => getPressForm())
-      .then((res) => res.json())
-      .then((res) => {
-        return Promise.all(
-          res.map((item) =>
-            addStamp({
-              color: item.color,
-              comment: item.comment,
-              lastEdited: item.lastEdited,
-              name: item.name,
-              number: item.number,
-              status: 'pressForm',
-            })
-              .then((res) => res.json())
-              .then((newId) =>
-                item.pressParts.map((part) =>
-                  addPartsToStamp({
-                    amount: part.amount,
-                    comment: part.comment,
-                    controll: part.controll,
-                    cuttingDimension: part.cuttingDimension,
-                    erosion: part.erosion,
-                    grinding: part.grinding,
-                    harding: part.harding,
-                    location: part.location,
-                    milling: part.milling,
-                    name: part.name,
-                    number: part.number,
-                    color: part.color,
-                    riggingId: newId.id,
-                  }),
-                ),
-              ),
-          ),
-        )
-      })
-      .then(() => getMachine())
-      .then((res) => res.json())
-      .then((res) => {
-        return Promise.all(
-          res.map((item) =>
-            addStamp({
-              color: item.color,
-              comment: item.comment,
-              lastEdited: item.lastEdited,
-              name: item.name,
-              number: item.number,
-              status: 'machine',
-            })
-              .then((res) => res.json())
-              .then((newId) =>
-                item.benchParts.map((part) =>
-                  addPartsToStamp({
-                    amount: part.amount,
-                    comment: part.comment,
-                    controll: part.controll,
-                    cuttingDimension: part.cuttingDimension,
-                    erosion: part.erosion,
-                    grinding: part.grinding,
-                    harding: part.harding,
-                    location: part.location,
-                    milling: part.milling,
-                    name: part.name,
-                    number: part.number,
-                    color: part.color,
-                    riggingId: newId.id,
-                  }),
-                ),
-              ),
-          ),
-        )
-      })
-      .then(() => getParts())
-      .then((res) => res.json())
-      .then((res) => {
-        return Promise.all(
-          res.map((item) =>
-            addStamp({
-              color: item.color,
-              comment: item.comment,
-              lastEdited: item.lastEdited,
-              name: item.name,
-              number: item.number,
-              status: 'parts',
-            })
-              .then((res) => res.json())
-              .then((newId) =>
-                item.detailParts.map((part) =>
-                  addPartsToStamp({
-                    amount: part.amount,
-                    comment: part.comment,
-                    controll: part.controll,
-                    cuttingDimension: part.cuttingDimension,
-                    erosion: part.erosion,
-                    grinding: part.grinding,
-                    harding: part.harding,
-                    location: part.location,
-                    milling: part.milling,
-                    name: part.name,
-                    number: part.number,
-                    color: part.color,
-                    riggingId: newId.id,
-                  }),
-                ),
-              ),
-          ),
-        )
-      })
-      .then(() => {
-        setIsLoading(false)
-        loadStamps()
-      })
-      .catch((error) => {
-        console.log(error)
-        setIsLoading(false)
-      })
   }
 
   return (
@@ -232,15 +94,6 @@ const Stamp = (props) => {
           </div>
         </div>
         <div className="main-window__info-panel">
-          {props.userHasAccess(['ROLE_ADMIN']) && (
-            <Button
-              inverted
-              isLoading={isLoading}
-              text="Скопировать все сюда"
-              className="main-window__button main-window__button--inverted"
-              onClick={handleCopyDataToStamp}
-            />
-          )}
           <div className="main-window__amount_table">
             Всего: {stamps.length} записей
           </div>
