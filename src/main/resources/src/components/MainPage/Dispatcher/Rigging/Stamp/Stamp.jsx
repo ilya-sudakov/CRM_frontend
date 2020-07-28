035986987864ba44a54a737465d9f85ac753dead
ilyasudakov <ilyasudakov@inbox.ru>
2020-07-28 21:25:50 +0300

    update copyAll function

--- src/main/resources/src/components/MainPage/Dispatcher/Rigging/Stamp/Stamp.jsx
+++ src/main/resources/src/components/MainPage/Dispatcher/Rigging/Stamp/Stamp.jsx
@@ -4,12 +4,20 @@ import './Stamp.scss'
 import '../../../../../utils/MainWindow/MainWindow.scss'
 import TableView from '../RiggingComponents/TableView/TableView.jsx'
 import {
+  getStamp,
   getStampById,
   deletePartsFromStamp,
   deleteStamp,
+  addStamp,
+  addPartsToStamp,
   getStampsByStatus,
+  editStamp,
 } from '../../../../../utils/RequestsAPI/Rigging/Stamp.jsx'
 import FloatingPlus from '../../../../../utils/MainWindow/FloatingPlus/FloatingPlus.jsx'
+import { getPressForm } from '../../../../../utils/RequestsAPI/Rigging/PressForm.jsx'
+import { getMachine } from '../../../../../utils/RequestsAPI/Rigging/Machine.jsx'
+import { getParts } from '../../../../../utils/RequestsAPI/Parts.jsx'
+import Button from '../../../../../utils/Form/Button/Button.jsx'
 
 const Stamp = (props) => {
   const [stamps, setStamps] = useState([])
@@ -30,6 +38,7 @@ const Stamp = (props) => {
   const loadStamps = (signal) => {
     setIsLoading(true)
     getStampsByStatus('stamp', signal)
+      // getStamp(signal)
       .then((res) => res.json())
       .then((res) => {
         setIsLoaded(true)
@@ -53,6 +62,54 @@ const Stamp = (props) => {
       )
       .then(() => deleteStamp(id))
       .then(() => loadStamps())
+  }
+
+  const handleCopyDataToStamp = () => {
+    setIsLoading(true)
+    return getParts()
+      .then((res) => res.json())
+      .then((res) => {
+        return Promise.all(
+          res.map((item) =>
+            addStamp({
+              color: item.color,
+              comment: item.comment,
+              lastEdited: item.lastEdited,
+              name: item.name,
+              number: item.number,
+              status: 'parts',
+            })
+              .then((res) => res.json())
+              .then((newId) =>
+                item.detailParts.map((part) =>
+                  addPartsToStamp({
+                    amount: part.amount,
+                    comment: part.comment,
+                    controll: part.controll,
+                    cuttingDimension: part.cuttingDimension,
+                    erosion: part.erosion,
+                    grinding: part.grinding,
+                    harding: part.harding,
+                    location: part.location,
+                    milling: part.milling,
+                    name: part.name,
+                    number: part.number,
+                    color: part.color,
+                    riggingId: newId.id,
+                  }),
+                ),
+              ),
+          ),
+        )
+      })
+      .then(() => {
+        setIsLoading(false)
+        loadStamps()
+      })
+      .catch((error) => {
+        console.log(error)
+        setIsLoading(false)
+      })
   }
 
   return (
@@ -94,6 +151,15 @@ const Stamp = (props) => {
           </div>
         </div>
         <div className="main-window__info-panel">
+          {props.userHasAccess(['ROLE_ADMIN']) && (
+            <Button
+              inverted
+              isLoading={isLoading}
+              text="Скопировать детали"
+              className="main-window__button main-window__button--inverted"
+              onClick={handleCopyDataToStamp}
+            />
+          )}
           <div className="main-window__amount_table">
             Всего: {stamps.length} записей
           </div>
