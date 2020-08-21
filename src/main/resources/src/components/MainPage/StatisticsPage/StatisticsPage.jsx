cbb2dcb6a57c1e613d3d3bd4bdc50d777e9458b5
ilyasudakov <ilyasudakov@inbox.ru>
2020-08-21 19:44:15 +0300

    remove transitions from sidemenu, add Panel component to Statistics

--- src/main/resources/src/components/MainPage/StatisticsPage/StatisticsPage.jsx
+++ src/main/resources/src/components/MainPage/StatisticsPage/StatisticsPage.jsx
@@ -6,9 +6,36 @@ const StatisticsPage = (props) => {
     <div className="statistics">
       <div className="main-window">
         <div className="main-window__title">Статистика</div>
+        <div className="statistics__row">
+          <SmallPanel percentage={2.57} />
+          <SmallPanel percentage={-50.3} />
+        </div>
       </div>
     </div>
   )
 }
 
 export default StatisticsPage
+
+const SmallPanel = (props) => {
+  return (
+    <div className="panel">
+      <div className="panel__category">
+        <span>Заказы</span>
+      </div>
+      <div className="panel__value">5,543</div>
+      <div
+        className={`panel__difference panel__difference--${
+          props.percentage < 0 ? 'negative' : 'positive'
+        }`}
+      >
+        <div className="panel__arrow"></div>
+        <div className="panel__percentage">
+          <span></span>
+          {`${Math.abs(props.percentage)}%`}
+        </div>
+        <div className="panel__time-period">С прошлого месяца</div>
+      </div>
+    </div>
+  )
+}
