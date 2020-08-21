df177ad4b02c7f0f93b97a5a13b38fc99fda6767
ilyasudakov <ilyasudakov@inbox.ru>
2020-08-21 22:47:46 +0300

    add requests statistics

--- src/main/resources/src/components/MainPage/StatisticsPage/StatisticsPage.jsx
+++ src/main/resources/src/components/MainPage/StatisticsPage/StatisticsPage.jsx
@@ -1,44 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './StatisticsPage.scss'
 
-import ClientsIcon from '../../../../../../../assets/sidemenu/client.inline.svg'
-import BoxIcon from '../../../../../../../assets/sidemenu/box.inline.svg'
-import TruckIcon from '../../../../../../../assets/sidemenu/truck.inline.svg'
+import { getRequests } from '../../../utils/RequestsAPI/Requests.jsx'
 
-const StatisticsPage = (props) => {
+import ClientsIcon from '../../../../../../../assets/sidemenu/client.inline.svg'
+
+const StatisticsPage = () => {
+  const [requestsStats, setRequestsStats] = useState({
+    category: 'Заявки',
+    percentage: 0,
+    value: null,
+    linkTo: '/requests',
+    isLoaded: false,
+    timePeriod: 'От прошлого месяца',
+    renderIcon: () => (
+      <ClientsIcon className="panel__img panel__img--requests" />
+    ),
+  })
+
+  const getRequestStats = () => {
+    !requestsStats.isLoaded &&
+      getRequests()
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res)
+          let curMonthQuantity = 0
+          let prevMonthQuantity = 0
+
+          //check prev month
+          let temp = res.filter((request) => {
+            const date = new Date(request.date)
+            if (
+              date.getMonth() === new Date(new Date().setDate(0)).getMonth()
+            ) {
+              prevMonthQuantity++
+              return false
+            }
+            return true
+          })
+          temp.map((request) => {
+            const date = new Date(request.date)
+            if (date.getMonth() === new Date().getMonth()) {
+              curMonthQuantity++
+            }
+          })
+
+          setRequestsStats((requestsStats) => ({
+            ...requestsStats,
+            isLoaded: true,
+            value: curMonthQuantity,
+            percentage:
+              Math.floor((curMonthQuantity / prevMonthQuantity) * 100 * 100) /
+              100,
+          }))
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+  }
+
+  useEffect(() => {
+    getRequestStats()
+  }, [])
+
   return (
     <div className="statistics">
       <div className="main-window">
         <div className="main-window__title">Статистика</div>
         <div className="statistics__row">
-          <SmallPanel
-            renderIcon={() => (
-              <ClientsIcon className="panel__img panel__img--requests" />
-            )}
-            category="Заказы"
-            percentage={2.57}
-            value={5543}
-            linkTo="/requests"
-          />
-          <SmallPanel
-            category="Посещения"
-            renderIcon={() => (
-              <BoxIcon className="panel__img panel__img--visits" />
-            )}
-            percentage={-50.3}
-            value={5043}
-            linkTo="/requests"
-          />
-          <SmallPanel
-            category="Звонки"
-            renderIcon={() => (
-              <TruckIcon className="panel__img panel__img--calls" />
-            )}
-            percentage={0}
-            value={3543}
-            linkTo="/requests"
-          />
+          <SmallPanel {...requestsStats} />
         </div>
       </div>
     </div>
@@ -51,29 +82,34 @@ const SmallPanel = (props) => {
   const Icon = props.icon
 
   return (
-    <Link className="panel" to={props.linkTo || '/'}>
+    <Link
+      className={`panel ${props.isLoaded ? '' : 'panel--placeholder'}`}
+      to={props.linkTo || '/'}
+    >
       <div className="panel__category">
         <span>{props.category || 'Категория'}</span>
         {props.renderIcon ? (
           <div className="panel__icon">{props.renderIcon()}</div>
         ) : null}
       </div>
-      <div className="panel__value">{props.value}</div>
+      <div className="panel__value">{props.value || ''}</div>
       <div
         className={`panel__difference panel__difference--${
-          props.percentage < 0
-            ? 'negative'
-            : props.percentage === 0
+          props.percentage === 0 || !props.isLoaded
             ? 'equal'
+            : props.percentage < 0
+            ? 'negative'
             : 'positive'
         }`}
       >
         <div className="panel__arrow"></div>
         <div className="panel__percentage">
           <span></span>
-          {`${Math.abs(props.percentage)}%`}
+          {props.isLoaded ? `${Math.abs(props.percentage)}%` : ''}
         </div>
-        <div className="panel__time-period">От прошлого месяца</div>
+        <div className="panel__time-period">
+          {props.isLoaded ? props.timePeriod : ''}
+        </div>
       </div>
     </Link>
   )
