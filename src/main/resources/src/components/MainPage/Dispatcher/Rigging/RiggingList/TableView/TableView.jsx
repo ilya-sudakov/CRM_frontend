ec5fc20456c7b9d8430450c9d6d535fdb03fc936
ilyasudakov <ilyasudakov@inbox.ru>
2020-08-24 14:10:20 +0300

    update RiggingList animation

--- src/main/resources/src/components/MainPage/Dispatcher/Rigging/RiggingList/TableView/TableView.jsx
+++ src/main/resources/src/components/MainPage/Dispatcher/Rigging/RiggingList/TableView/TableView.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import './TableView.scss'
-
-import TableDataLoading from '../../../../../../utils/TableView/TableDataLoading/TableDataLoading.jsx'
 import { UserContext } from '../../../../../../App.js'
 
 import chevronDownSVG from '../../../../../../../../../../assets/tableview/chevron-down.svg'
 import editIcon from '../../../../../../../../../../assets/tableview/edit.svg'
 import { rigStatuses } from '../../RiggingComponents/rigsVariables.js'
+import PlaceholderLoading from '../../../../../../utils/TableView/PlaceholderLoading/PlaceholderLoading.jsx'
 
 const TableView = (props) => {
   const [workshops, setWorkshops] = useState([
@@ -84,76 +83,80 @@ const TableView = (props) => {
                 <span>Статус</span>
                 <div className="main-window__actions">Действия</div>
               </div>
-              {props.isLoading && (
-                <TableDataLoading
-                  className="main-window__list-item"
-                  minHeight="40px"
+              {props.isLoading ? (
+                <PlaceholderLoading
+                  itemClassName="main-window__list-item"
+                  minHeight="35px"
+                  items={3}
                 />
-              )}
-              {props.data
-                .filter(
-                  (item) =>
-                    item.location === workshop.name ||
-                    workshop.alternatives.find(
-                      (oldName) => oldName === item.location,
-                    ) !== undefined,
-                )
-                .map((item) => {
-                  if (!workshop.isMinimized) {
-                    return (
-                      <div className="main-window__list-item">
-                        <span>
-                          <div className="main-window__mobile-text">
-                            Артикул:
-                          </div>
-                          {item.number}
-                        </span>
-                        <span>
-                          <div className="main-window__mobile-text">
-                            Название:
-                          </div>
-                          {item.name}
-                        </span>
-                        <span>
-                          <div className="main-window__mobile-text">
-                            Кол-во:
-                          </div>
-                          {item.amount}
-                        </span>
-                        <span>
-                          <div className="main-window__mobile-text">
-                            Комментарий:
-                          </div>
-                          {item.comment}
-                        </span>
-                        <span
-                          className={`main-window__list-item--${
-                            rigStatuses[item.color || 'production']?.className
-                          }`}
-                        >
-                          <div className="main-window__mobile-text">Статус</div>
-                          {rigStatuses[item.color || 'production'].name}
-                        </span>
-                        <div className="main-window__actions">
-                          <Link
-                            className="main-window__action"
-                            to={`/dispatcher/rigging/${
-                              riggingNames[item.type.toLowerCase()]
-                            }/edit-part/${item.id}/${item.id}`}
-                            title="Редактирование"
+              ) : (
+                props.data
+                  .filter(
+                    (item) =>
+                      item.location === workshop.name ||
+                      workshop.alternatives.find(
+                        (oldName) => oldName === item.location,
+                      ) !== undefined,
+                  )
+                  .map((item) => {
+                    if (!workshop.isMinimized) {
+                      return (
+                        <div className="main-window__list-item">
+                          <span>
+                            <div className="main-window__mobile-text">
+                              Артикул:
+                            </div>
+                            {item.number}
+                          </span>
+                          <span>
+                            <div className="main-window__mobile-text">
+                              Название:
+                            </div>
+                            {item.name}
+                          </span>
+                          <span>
+                            <div className="main-window__mobile-text">
+                              Кол-во:
+                            </div>
+                            {item.amount}
+                          </span>
+                          <span>
+                            <div className="main-window__mobile-text">
+                              Комментарий:
+                            </div>
+                            {item.comment}
+                          </span>
+                          <span
+                            className={`main-window__list-item--${
+                              rigStatuses[item.color || 'production']?.className
+                            }`}
                           >
-                            <img
-                              className="main-window__img"
-                              src={editIcon}
-                              alt=""
-                            />
-                            {/* Редактирование */}
-                          </Link>
+                            <div className="main-window__mobile-text">
+                              Статус
+                            </div>
+                            {rigStatuses[item.color || 'production'].name}
+                          </span>
+                          <div className="main-window__actions">
+                            <Link
+                              className="main-window__action"
+                              to={`/dispatcher/rigging/${
+                                riggingNames[item.type.toLowerCase()]
+                              }/edit-part/${item.id}/${item.id}`}
+                              title="Редактирование"
+                            >
+                              <img
+                                className="main-window__img"
+                                src={editIcon}
+                                alt=""
+                              />
+                              {/* Редактирование */}
+                            </Link>
+                          </div>
                         </div>
-                      </div>
-                    )
-                  }
-                })}
+                      )
+                    }
+                  })
+              )}
             </>
           ))}
       </div>
