8637dbcd182f89d30a7a035b35a15bdf373f06d1
ilyasudakov <ilyasudakov@inbox.ru>
2020-09-06 19:19:42 +0300

    add loading to workshops when rendering pdf

--- src/main/resources/src/components/MainPage/Requests/Requests.jsx
+++ src/main/resources/src/components/MainPage/Requests/Requests.jsx
@@ -410,8 +410,8 @@ const Requests = (props) => {
               <select onChange={changeSortOrder}>
                 <option value="date desc">По дате (убыв.)</option>
                 <option value="date asc">По дате (возр.)</option>
-                <option value="codeWord asc">По клиенту (А-Я)</option>
-                <option value="codeWord desc">По клиенту (Я-А)</option>
+                {/* <option value="codeWord asc">По клиенту (А-Я)</option>
+                <option value="codeWord desc">По клиенту (Я-А)</option> */}
                 <option value="shippingDate desc">
                   По дате отгрузки (убыв.)
                 </option>
