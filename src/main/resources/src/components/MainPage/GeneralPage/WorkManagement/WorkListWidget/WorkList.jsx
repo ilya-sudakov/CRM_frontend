fdab78e848457258cc4e37608934348d68bbbe54
ilyasudakov <ilyasudakov@inbox.ru>
2020-11-24 17:01:59 +0300

    refactor old code for WorkListWidget

---
+++ src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/WorkListWidget/WorkList.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import './WorkListWidget.scss'
+import { numberToString } from '../../../../../utils/functions.jsx'
+import {
+  filterEmployeesObject,
+  roundUpWorkHours,
+  sortEmployeesObject,
+} from './functions.js'
+
+const WorkList = ({ workshops, employees, employeesMap, userContext }) => {
+  return (
+    <div className="work-list-widget__list">
+      {workshops.map((workshop) => {
+        if (
+          userContext.userHasAccess(workshop.visibility) &&
+          Object.entries(employees).filter((employee) => {
+            const item = employee[1]
+            if (item.workshop === workshop.name) {
+              return true
+            }
+          }).length > 0
+        ) {
+          return (
+            <>
+              <div className="work-list-widget__workshop-item">
+                {workshop.name}
+              </div>
+              {sortEmployeesObject(
+                filterEmployeesObject(Object.entries(employees), workshop),
+              ).map((employee) => {
+                const item = employee[1]
+                return <ListItem item={item} employeesMap={employeesMap} />
+              })}
+            </>
+          )
+        }
+      })}
+    </div>
+  )
+}
+
+export default WorkList
+
+const ListItem = ({ item, employeesMap }) => {
+  return (
+    <div index={item.id} className="work-list-widget__item">
+      <div>{item.lastName + ' ' + item.name + ' ' + item.middleName}</div>
+      <div>
+        {employeesMap !== undefined &&
+          roundUpWorkHours(employeesMap[item.id]?.hours) +
+            ' ' +
+            numberToString(
+              Number.parseInt(roundUpWorkHours(employeesMap[item.id]?.hours)),
+              ['час', 'часа', 'часов'],
+            )}
+      </div>
+    </div>
+  )
+}
