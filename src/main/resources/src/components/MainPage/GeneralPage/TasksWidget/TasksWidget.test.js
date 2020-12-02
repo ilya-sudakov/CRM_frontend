4f5594125ca4c56041b9c5a7d13ee6666fa595e3
ilyasudakov <ilyasudakov@inbox.ru>
2020-12-02 14:40:05 +0300

    add tests for TasksWidget

--- src/main/resources/src/components/MainPage/GeneralPage/TasksWidget/TasksWidget.test.js
+++ src/main/resources/src/components/MainPage/GeneralPage/TasksWidget/TasksWidget.test.js
@@ -2,10 +2,39 @@ import React from 'react'
 import TasksWidget from './TasksWidget.jsx'
 import '@testing-library/jest-dom/extend-expect'
 import { render, cleanup } from '@testing-library/react'
+import UserContext, { App } from '../../../../App.js'
+import { MemoryRouter } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
 
 afterEach(cleanup)
 
-it('matches snapshot', () => {
-  // const { asFragment } = render(<TasksWidget />)
-  // expect(asFragment()).toMatchSnapshot()
+const mockHistoryPush = jest.fn()
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}))
+
+const renderWithRouterAndContext = (component) => {
+  const history = createMemoryHistory()
+  return {
+    ...render(
+      <MemoryRouter history={history}>
+        {<App value={UserContext}>{component}</App>}
+      </MemoryRouter>,
+    ),
+  }
+}
+
+describe('TasksWidget component', () => {
+  it('renders', () => {
+    renderWithRouterAndContext(<TasksWidget />)
+  })
+
+  it('matches snapshot', () => {
+    const { asFragment } = renderWithRouterAndContext(<TasksWidget />)
+    expect(asFragment()).toMatchSnapshot()
+  })
 })
