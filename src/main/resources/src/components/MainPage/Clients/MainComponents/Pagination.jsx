89cea2939775ec4ffcf54ad69d8c31afbba08842
ilyasudakov <ilyasudakov@inbox.ru>
2020-12-04 21:25:16 +0300

    fix pagination bug

--- src/main/resources/src/components/MainPage/Clients/MainComponents/Pagination.jsx
+++ src/main/resources/src/components/MainPage/Clients/MainComponents/Pagination.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Pagination = ({
   itemsPerPage,
@@ -20,36 +20,34 @@ const Pagination = ({
         className="main-window__page-number main-window__page-number--skip"
         onClick={() => {
           if (curPage > 1) {
-            const item = curPage - 1
-            const lastPage = Math.floor(itemsCount / itemsPerPage)
-            setCurPage(item)
+            const item = curPage - 1;
+            const lastPage = Math.floor(itemsCount / itemsPerPage);
+            setCurPage(item);
             if (lastPage > 5) {
               if (pagination.indexOf(item) === 0 && item !== 1) {
-                let temp = []
+                let temp = [];
                 for (
                   let i = pagination[0] - 1;
-                  i <= lastPage &&
-                  i <= pagination[pagination.length - 1] - 1;
+                  i <= lastPage && i <= pagination[pagination.length - 1] - 1;
                   i++
                 ) {
-                  temp.push(i)
+                  temp.push(i);
                 }
-                return setPagination(temp)
+                return setPagination(temp);
               }
               if (
                 pagination.indexOf(item) === pagination.length - 1 &&
                 item !== lastPage
               ) {
-                let temp = []
+                let temp = [];
                 for (
                   let i = pagination[0] + 1;
-                  i <= lastPage &&
-                  i <= pagination[pagination.length - 1] + 1;
+                  i <= lastPage && i <= pagination[pagination.length - 1] + 1;
                   i++
                 ) {
-                  temp.push(i)
+                  temp.push(i);
                 }
-                return setPagination(temp)
+                return setPagination(temp);
               }
             }
           }
@@ -57,39 +55,39 @@ const Pagination = ({
       >
         Пред
       </div>
-      {curPage >= 5 && searchQuery === '' && (
+      {curPage >= 5 && searchQuery === "" && (
         <>
           <div
             className="main-window__page-number"
             onClick={() => {
-              const item = 1
-              const lastPage = Math.floor(itemsCount / itemsPerPage)
-              setCurPage(item)
+              const item = 1;
+              const lastPage = Math.floor(itemsCount / itemsPerPage);
+              setCurPage(item);
               if (lastPage > 5) {
                 if (pagination.indexOf(item) === 0 && item !== 1) {
-                  let temp = []
+                  let temp = [];
                   for (
                     let i = pagination[0] - 1;
                     i <= lastPage && i <= pagination[pagination.length - 1] - 1;
                     i++
                   ) {
-                    temp.push(i)
+                    temp.push(i);
                   }
-                  return setPagination(temp)
+                  return setPagination(temp);
                 }
                 if (
                   pagination.indexOf(item) === pagination.length - 1 &&
                   item !== lastPage
                 ) {
-                  let temp = []
+                  let temp = [];
                   for (
                     let i = pagination[0] + 1;
                     i <= lastPage && i <= pagination[pagination.length - 1] + 1;
                     i++
                   ) {
-                    temp.push(i)
+                    temp.push(i);
                   }
-                  return setPagination(temp)
+                  return setPagination(temp);
                 }
               }
             }}
@@ -104,38 +102,38 @@ const Pagination = ({
           <div
             className={
               curPage == item
-                ? 'main-window__page-number main-window__page-number--active'
-                : 'main-window__page-number'
+                ? "main-window__page-number main-window__page-number--active"
+                : "main-window__page-number"
             }
             onClick={() => {
-              setCurPage(item)
+              setCurPage(item);
               if (Math.floor(itemsCount / itemsPerPage) > 5) {
                 if (pagination.indexOf(item) === 0 && item !== 1) {
-                  let temp = []
+                  let temp = [];
                   for (
                     let i = pagination[0] - 1;
                     i <= Math.floor(itemsCount / itemsPerPage) &&
                     i <= pagination[pagination.length - 1] - 1;
                     i++
                   ) {
-                    temp.push(i)
+                    temp.push(i);
                   }
-                  return setPagination(temp)
+                  return setPagination(temp);
                 }
                 if (
                   pagination.indexOf(item) === pagination.length - 1 &&
                   item !== Math.ceil(itemsCount / itemsPerPage)
                 ) {
-                  let temp = []
+                  let temp = [];
                   for (
                     let i = pagination[0] + 1;
                     i <= Math.ceil(itemsCount / itemsPerPage) &&
                     i <= pagination[pagination.length - 1] + 1;
                     i++
                   ) {
-                    temp.push(i)
+                    temp.push(i);
                   }
-                  return setPagination(temp)
+                  return setPagination(temp);
                 }
               }
             }}
@@ -145,68 +143,68 @@ const Pagination = ({
               из {Math.ceil(itemsCount / itemsPerPage)}
             </div>
           </div>
-        )
+        );
       })}
       {curPage <= Math.ceil(itemsCount / itemsPerPage) - 2 &&
         Math.ceil(itemsCount / itemsPerPage) > 5 &&
         pagination.find(
-          (item) => item === Math.ceil(itemsCount / itemsPerPage),
+          (item) => item === Math.ceil(itemsCount / itemsPerPage)
         ) === undefined &&
-        searchQuery === '' && (
+        searchQuery === "" && (
           <>
             <span>...</span>
             <div
               className="main-window__page-number"
               onClick={() => {
-                const item = Math.ceil(itemsCount / itemsPerPage)
-                setCurPage(item)
+                const item = Math.ceil(itemsCount / itemsPerPage);
+                setCurPage(item);
                 // console.log(item)
                 if (item > 5) {
-                  let temp = []
+                  let temp = [];
                   for (let i = item - 5; i <= item; i++) {
-                    temp.push(i)
+                    temp.push(i);
                   }
-                  return setPagination(temp)
+                  return setPagination(temp);
                 }
               }}
             >
-              {item}
+              {Math.ceil(itemsCount / itemsPerPage)}
             </div>
           </>
         )}
       <div
         className="main-window__page-number main-window__page-number--skip"
         onClick={() => {
-          const maxPage = Math.ceil(itemsCount / itemsPerPage)
+          const maxPage = Math.ceil(itemsCount / itemsPerPage);
 
           if (curPage < maxPage) {
-            const item = curPage + 1
-            setCurPage(item)
+            const item = curPage + 1;
+            setCurPage(item);
             if (maxPage >= 5) {
               if (pagination.indexOf(item) === 0 && item !== 1) {
-                let temp = []
+                let temp = [];
                 for (
                   let i = pagination[0] - 1;
                   i <= maxPage && i <= pagination[pagination.length - 1] - 1;
                   i++
                 ) {
-                  temp.push(i)
+                  temp.push(i);
                 }
-                return setPagination(temp)
+                return setPagination(temp);
               }
               if (
                 pagination.indexOf(item) === pagination.length - 1 &&
                 item !== maxPage
               ) {
-                let temp = []
+                let temp = [];
                 for (
                   let i = pagination[0] + 1;
                   i <= maxPage && i <= pagination[pagination.length - 1] + 1;
                   i++
                 ) {
-                  temp.push(i)
+                  temp.push(i);
                 }
-                return setPagination(temp)
+                return setPagination(temp);
               }
             }
           }
@@ -215,10 +213,10 @@ const Pagination = ({
         След
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
 
 const ItemsPerPage = ({ itemsPerPage, setItemsPerPage }) => {
   return (
@@ -227,8 +225,8 @@ const ItemsPerPage = ({ itemsPerPage, setItemsPerPage }) => {
       <select
         value={itemsPerPage}
         onChange={(event) => {
-          const value = event.target.value
-          setItemsPerPage(value)
+          const value = event.target.value;
+          setItemsPerPage(value);
         }}
       >
         <option>20</option>
@@ -236,5 +234,5 @@ const ItemsPerPage = ({ itemsPerPage, setItemsPerPage }) => {
         <option>10</option>
       </select>
     </div>
-  )
-}
+  );
+};
