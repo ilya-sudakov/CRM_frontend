331d70950a86aedcdd6331949893f6509413400e
ilyasudakov <ilyasudakov@inbox.ru>
2020-12-11 13:48:15 +0300

    update ErrorMessage styles

--- src/main/resources/src/utils/Form/ErrorMessage/ErrorMessage.jsx
+++ src/main/resources/src/utils/Form/ErrorMessage/ErrorMessage.jsx
@@ -1,54 +1,52 @@
-import React, { useEffect, useCallback } from 'react'
-// import { Link } from 'react-router-dom';
-import './ErrorMessage.scss'
+import React, { useEffect, useCallback } from "react";
+import Button from "../Button/Button.jsx";
+import "./ErrorMessage.scss";
 
 const ErrorMessage = (props) => {
   const clickOnErrorWindow = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (
-      !(e.target.classList[0] === 'window_error') &&
-      !e.target.classList.contains('window_error__exit') &&
-      !e.target.classList.contains('window_error__bar') &&
-      !e.target.classList.contains('window_error__button')
+      !(e.target.classList[0] === "window_error") &&
+      !e.target.classList.contains("window_error__exit") &&
+      !e.target.classList.contains("window_error__bar") &&
+      !e.target.classList.contains("window_error__button")
     ) {
-      props.setShowError(true)
+      props.setShowError(true);
     } else {
-      props.setShowError(false)
+      props.setShowError(false);
     }
-  }
+  };
 
   const handleCloseWindow = useCallback((event) => {
-    if (event.key === 'Enter' || event.key === 'Escape') {
-      event.preventDefault()
-      props.setShowError(false)
+    if (event.key === "Enter" || event.key === "Escape") {
+      event.preventDefault();
+      props.setShowError(false);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    document.addEventListener('keydown', handleCloseWindow, false)
+    document.addEventListener("keydown", handleCloseWindow, false);
     return () => {
-      document.removeEventListener('keydown', handleCloseWindow, false)
-    }
-  }, [])
+      document.removeEventListener("keydown", handleCloseWindow, false);
+    };
+  }, []);
 
   return (
-    //Окно для добавления продукции по категориям
     <div
       className={
-        props.showError ? 'window_error' : 'window_error window_error--hidden'
+        props.showError ? "window_error" : "window_error window_error--hidden"
       }
       onClick={clickOnErrorWindow}
     >
       <div
         className={
           props.showError
-            ? 'window_error__content'
-            : 'window_error__content window_error__content--hidden'
+            ? "window_error__content"
+            : "window_error__content window_error__content--hidden"
         }
       >
         <div className="window_error__title">
           Ошибка!
-          {/* <Link to="/products/new" className="window_error__button">Создать продукцию</Link> */}
           <div className="window_error__exit" onClick={clickOnErrorWindow}>
             <div
               className="window_error__bar"
@@ -61,15 +59,14 @@ const ErrorMessage = (props) => {
           </div>
         </div>
         <div className="window_error__message">{props.message}</div>
-        <div
+        <Button
           className="window_error__button window_error__button--submit"
           onClick={clickOnErrorWindow}
-        >
-          ОК
-        </div>
+          text="ОК"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorMessage
+export default ErrorMessage;
