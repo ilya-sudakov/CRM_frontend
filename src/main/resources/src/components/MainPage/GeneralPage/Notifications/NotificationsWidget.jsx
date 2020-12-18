fed723d8038d6d167942dbb250ab939b3576a52d
ilyasudakov <ilyasudakov@inbox.ru>
2020-12-18 22:04:08 +0300

    update NotificationsWidget header content

--- src/main/resources/src/components/MainPage/GeneralPage/Notifications/NotificationsWidget.jsx
+++ src/main/resources/src/components/MainPage/GeneralPage/Notifications/NotificationsWidget.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import deleteSVG from "../../../../../../../../assets/select/delete.svg";
+import openWidget from "../../../../../../../../assets/tableview/bx-window-open.svg";
 import "./NotificationsWidget.scss";
 import Widget from "../Widget/Widget.jsx";
 import NotificationsList from "./NotificationsList/NotificationsList.jsx";
@@ -16,6 +18,12 @@ const NotificationsWidget = () => {
     <Widget
       className="notifications-widget"
       title="Уведомления"
+      subTitle="Сотрудники"
+      linkTo={{
+        address: "/dispatcher/employees",
+        text: "Перейти",
+        img: openWidget,
+      }}
       content={
         <NotificationsList
           notifications={notifications}
