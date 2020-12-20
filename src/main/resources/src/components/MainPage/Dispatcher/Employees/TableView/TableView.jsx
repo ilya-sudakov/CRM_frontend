7495ce26b701595acd9b1e40ae5eec9287a0e22e
ilyasudakov <ilyasudakov@inbox.ru>
2020-12-20 19:58:14 +0300

    fix employee search bug

--- src/main/resources/src/components/MainPage/Dispatcher/Employees/TableView/TableView.jsx
+++ src/main/resources/src/components/MainPage/Dispatcher/Employees/TableView/TableView.jsx
@@ -34,7 +34,7 @@ const TableView = (props) => {
         item.name.toLowerCase().includes(query) ||
         item.middleName.toLowerCase().includes(query) ||
         item.id.toString().includes(query) ||
-        item.yearOfBirth.toString().includes(query) ||
+        (item.yearOfBirth && item.yearOfBirth.toString().includes(query)) ||
         item.citizenship.toLowerCase().includes(query) ||
         item.workshop.toLowerCase().includes(query) ||
         item.position.toLowerCase().includes(query) ||
