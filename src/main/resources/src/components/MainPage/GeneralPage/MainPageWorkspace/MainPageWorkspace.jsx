2b55728cfff69c26a01e636e488b97f25f34938f
ilyasudakov <ilyasudakov@inbox.ru>
2020-12-23 08:42:11 +0300

    allow dispatcher to see notifications widget

--- src/main/resources/src/components/MainPage/GeneralPage/MainPageWorkspace/MainPageWorkspace.jsx
+++ src/main/resources/src/components/MainPage/GeneralPage/MainPageWorkspace/MainPageWorkspace.jsx
@@ -11,7 +11,9 @@ const MainPageWorkspace = (props) => {
       <TasksWidget />
       <WorkManagement />
       {props.userHasAccess(["ROLE_ADMIN"]) && <GraphWidget />}
-      {props.userHasAccess(["ROLE_ADMIN"]) && <Notifications />}
+      {props.userHasAccess(["ROLE_ADMIN", "ROLE_DISPATCHER"]) && (
+        <Notifications />
+      )}
     </div>
   );
 };
