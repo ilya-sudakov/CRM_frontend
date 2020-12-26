ca1dbf375bfbd4f02d55f70ee82035187f9c6dfb
ilyasudakov <ilyasudakov@inbox.ru>
2020-12-26 09:34:09 +0300

    move ActionItem to separate file

---
+++ src/main/resources/src/utils/TableView/TableActions/Actions/ActionItem.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const defaultItem = {
+  isRendered: false,
+};
+
+const ActionItem = ({ item = defaultItem }) => {
+  const isRendered = item.isRendered === undefined || item.isRendered === true;
+  if (item.customElement) return !isRendered ? null : item.customElement;
+  if (!isRendered) return null;
+  if (item.link) {
+    return (
+      <Link
+        className="main-window__action"
+        to={item.link}
+        title={item.title}
+        target={item.openInNewTab ? "_blank" : ""}
+        rel={item.openInNewTab ? "noopener noreferrer" : ""}
+      >
+        {item.text}
+        <img className="main-window__img" src={item.imgSrc} />
+      </Link>
+    );
+  }
+  return (
+    <div
+      className="main-window__action"
+      onClick={item.onClick}
+      title={item.title}
+    >
+      {item.text}
+      <img className="main-window__img" src={item.imgSrc} />
+    </div>
+  );
+};
+
+export default ActionItem;
--- src/main/resources/src/utils/TableView/TableActions/TableActions.jsx
+++ src/main/resources/src/utils/TableView/TableActions/TableActions.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import DotsIcon from "../../../../../../../assets/sidemenu/more.inline.svg";
 import "./TableActions.scss";
-import { Link } from "react-router-dom";
+import ActionItem from "./Actions/ActionItem.jsx";
 
 const TableActions = ({ actionsList = [] }) => {
   const [isHidden, setIsHidden] = useState(true);
@@ -35,34 +35,4 @@ export default TableActions;
 
 TableActions.propTypes = {
   actionsList: PropTypes.array,
-};
-
-const ActionItem = ({ item }) => {
-  const isRendered = item.isRendered === undefined || item.isRendered === true;
-  if (item.customElement) return !isRendered ? null : item.customElement;
-  if (!isRendered) return null;
-  if (item.link) {
-    return (
-      <Link
-        className="main-window__action"
-        to={item.link}
-        title={item.title}
-        target={item.openInNewTab ? "_blank" : ""}
-        rel={item.openInNewTab ? "noopener noreferrer" : ""}
-      >
-        {item.text}
-        <img className="main-window__img" src={item.imgSrc} />
-      </Link>
-    );
-  }
-  return (
-    <div
-      className="main-window__action"
-      onClick={item.onClick}
-      title={item.title}
-    >
-      {item.text}
-      <img className="main-window__img" src={item.imgSrc} />
-    </div>
-  );
 };
