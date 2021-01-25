e2178309a9104db517442e038cf84f73dad8dd20
ilyasudakov <ilyasudakov@inbox.ru>
2021-01-25 11:41:43 +0300

    add CheckBox test cases

--- src/main/resources/src/components/SideMenu/SideMenu.test.js
+++ src/main/resources/src/components/SideMenu/SideMenu.test.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import { cleanup, screen } from '@testing-library/react'
-import SideMenu from './SideMenu.jsx'
-import '@testing-library/jest-dom/extend-expect'
-import { renderWithRouterAndContext } from '../../utils/testing/functions.js'
+import React from "react";
+import { cleanup, screen } from "@testing-library/react";
+import SideMenu from "./SideMenu.jsx";
+import "@testing-library/jest-dom/extend-expect";
+import { renderWithRouterAndContext } from "../../utils/testing/functions.js";
 
-describe('Sidemenu component', () => {
-  afterEach(cleanup)
+describe("Sidemenu component", () => {
+  afterEach(cleanup);
 
-  it('renders w/ router & context', () => {
-    renderWithRouterAndContext(<SideMenu />)
-  })
+  it("renders w/ router & context", () => {
+    renderWithRouterAndContext(<SideMenu />);
+  });
 
-  it('matches snapshot', () => {
-    const { asFragment } = renderWithRouterAndContext(<SideMenu />)
-    expect(asFragment()).toMatchSnapshot()
-  })
-})
+  it("matches snapshot", () => {
+    const { asFragment } = renderWithRouterAndContext(<SideMenu />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("is hidden", () => {
+    renderWithRouterAndContext(<SideMenu hidden={true} />);
+  });
+});
