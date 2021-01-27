724ee0f7e9d8c1f057148487427f49dfd31626a2
ilyasudakov <ilyasudakov@inbox.ru>
2021-01-27 16:47:15 +0300

    add test cases

--- src/main/resources/src/components/Header/Header.test.js
+++ src/main/resources/src/components/Header/Header.test.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { cleanup, screen } from "@testing-library/react";
+import { render, cleanup, fireEvent, screen } from "@testing-library/react";
 import Header from "./Header.jsx";
-import "@testing-library/jest-dom/extend-expect";
+// import "@testing-library/jest-dom/extend-expect";
 import { renderWithRouterAndContext } from "../../utils/testing/functions.js";
 
 describe("Header component", () => {
@@ -14,5 +14,24 @@ describe("Header component", () => {
   it("matches snapshot", () => {
     const { asFragment } = renderWithRouterAndContext(<Header />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("closes sidemenu", () => {
+    let sidemenuHidden = false;
+    renderWithRouterAndContext(
+      <Header
+        sidemenuHidden={sidemenuHidden}
+        setSidemenuHidden={(sidemenuHidden) =>
+          (sidemenuHidden = sidemenuHidden)
+        }
+      />
+    );
+    setTimeout(async () => {
+      const sidemenuButton = document.getElementsByClassName(
+        "header__sidemenu"
+      )[0];
+      fireEvent.click(sidemenuButton);
+      expect(sidemenuHidden).toBe(true);
+    }, 1000);
   });
 });
