f64eefb16af57805e04a325e30bf22c6e29f987a
ilyasudakov <ilyasudakov@inbox.ru>
2021-01-31 10:21:41 +0300

    add basic useSort hook

---
+++ src/main/resources/src/utils/hooks/useSort/useSort.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import useQuery from "../useQuery.js";
+
+const useSort = (
+  props = {} //default values
+) => {
+  const [sortOrder, setSortOrder] = useState(
+    props.sortOrder ?? {
+      curSort: "date",
+      date: "desc",
+    }
+  );
+
+  const changeSortOrder = (event) => {
+    const name = event.target.value.split(" ")[0];
+    const order = event.target.value.split(" ")[1];
+    return Object.assign({
+      curSort: name,
+      [name]: order,
+    });
+  };
+
+  useEffect(() => {}, [...changableParams]);
+
+  const defaultOptions = [
+    { value: "date desc", text: "По дате (убыв.)" },
+    { value: "date asc", text: "По дате (возр.)" },
+  ];
+
+  const sortPanel = (
+    <div className="main-window__sort-panel">
+      <select className="main-window__select" onChange={changeSortOrder}>
+        {(props.sortOptions ?? defaultOptions).map((item) => (
+          <option value={item.value}>{item.text}</option>
+        ))}
+      </select>
+    </div>
+  );
+
+  return {
+    sortOrder,
+    setSortOrder,
+    sortPanel,
+    changeSortOrder,
+  };
+};
+
+export default useSort;
