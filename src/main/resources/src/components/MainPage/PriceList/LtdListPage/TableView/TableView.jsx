c21b3c932b1ebe80f9158776730aed8390bea3d5
ilyasudakov <ilyasudakov@inbox.ru>
2021-02-12 21:45:46 +0300

    move Ltd List styles to separate file

--- src/main/resources/src/components/MainPage/PriceList/LtdListPage/TableView/TableView.jsx
+++ src/main/resources/src/components/MainPage/PriceList/LtdListPage/TableView/TableView.jsx
@@ -6,6 +6,8 @@ import editSVG from "../../../../../../../../../assets/tableview/edit.svg";
 import okSVG from "../../../../../../../../../assets/tableview/ok.svg";
 import PlaceholderLoading from "../../../../../utils/TableView/PlaceholderLoading/PlaceholderLoading.jsx";
 
+import "./TableView.scss";
+
 const TableView = ({
   data,
   deleteItem,
@@ -21,62 +23,64 @@ const TableView = ({
   }, [selectedLtd]);
 
   return (
-    <div className="main-window__list main-window__list--full">
-      <div className="main-window__list-item main-window__list-item--header">
-        <span>Название</span>
-        <span>Адрес</span>
-        <span>ИНН</span>
-        <div className="main-window__table-actions"></div>
-      </div>
-      {isLoading ? (
-        <PlaceholderLoading
-          itemClassName="main-window__list-item"
-          minHeight="30px"
-          items={3}
-        />
-      ) : (
-        data.map((item) => (
-          <div
-            className="main-window__list-item"
-            onClick={onSelect ? () => onSelect(item) : null}
-          >
-            <span>{item.name}</span>
-            <span>{item.legalAddress}</span>
-            <span>{item.inn}</span>
-            {onSelect ? (
-              <div className="main-window__table-actions">
-                <img
-                  onClick={() => onSelect(item)}
-                  className="main-window__img"
-                  src={okSVG}
+    <div className="ltd-tableview">
+      <div className="main-window__list main-window__list--full">
+        <div className="main-window__list-item main-window__list-item--header">
+          <span>Название</span>
+          <span>Адрес</span>
+          <span>ИНН</span>
+          <div className="main-window__table-actions"></div>
+        </div>
+        {isLoading ? (
+          <PlaceholderLoading
+            itemClassName="main-window__list-item"
+            minHeight="30px"
+            items={3}
+          />
+        ) : (
+          data.map((item) => (
+            <div
+              className="main-window__list-item"
+              onClick={onSelect ? () => onSelect(item) : null}
+            >
+              <span>{item.name}</span>
+              <span>{item.legalAddress}</span>
+              <span>{item.inn}</span>
+              {onSelect ? (
+                <div className="main-window__table-actions">
+                  <img
+                    onClick={() => onSelect(item)}
+                    className="main-window__img"
+                    src={okSVG}
+                  />
+                </div>
+              ) : (
+                <TableActions
+                  actionsList={[
+                    {
+                      title: "Редактирование",
+                      link: `/ltd-list/edit/${item.id}`,
+                      imgSrc: editSVG,
+                      isRendered: userHasAccess(["ROLE_ADMIN", "ROLE_MANAGER"]),
+                    },
+                    {
+                      customElement: (
+                        <DeleteItemAction
+                          title="Удаление заявки"
+                          onClick={() => deleteItem(item.id)}
+                        />
+                      ),
+                      isRendered:
+                        (deleteItem ? deleteItem : false) &&
+                        userHasAccess(["ROLE_ADMIN"]),
+                    },
+                  ]}
                 />
-              </div>
-            ) : (
-              <TableActions
-                actionsList={[
-                  {
-                    title: "Редактирование",
-                    link: `/ltd-list/edit/${item.id}`,
-                    imgSrc: editSVG,
-                    isRendered: userHasAccess(["ROLE_ADMIN", "ROLE_MANAGER"]),
-                  },
-                  {
-                    customElement: (
-                      <DeleteItemAction
-                        title="Удаление заявки"
-                        onClick={() => deleteItem(item.id)}
-                      />
-                    ),
-                    isRendered:
-                      (deleteItem ? deleteItem : false) &&
-                      userHasAccess(["ROLE_ADMIN"]),
-                  },
-                ]}
-              />
-            )}
-          </div>
-        ))
-      )}
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
