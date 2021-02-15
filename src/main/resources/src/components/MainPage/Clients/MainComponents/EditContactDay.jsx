b171aaef1b5763f5e63c85e1c2f9c87748074285
ilyasudakov <ilyasudakov@inbox.ru>
2021-02-15 14:15:28 +0300

    update modal styles for clients forms

--- src/main/resources/src/components/MainPage/Clients/MainComponents/EditContactDay.jsx
+++ src/main/resources/src/components/MainPage/Clients/MainComponents/EditContactDay.jsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import InputDate from '../../../../utils/Form/InputDate/InputDate.jsx'
-import Button from '../../../../utils/Form/Button/Button.jsx'
+import React, { useState, useEffect } from "react";
+import InputDate from "../../../../utils/Form/InputDate/InputDate.jsx";
+import Button from "../../../../utils/Form/Button/Button.jsx";
 
 const EditNextContactDate = (props) => {
-  const [date, setDate] = useState(new Date())
-  const [isLoading, setIsLoading] = useState(false)
+  const [date, setDate] = useState(new Date());
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = () => {
-    setIsLoading(true)
+    setIsLoading(true);
     props
       .editNextContactDate({
         nextDateContact: new Date(date).getTime() / 1000,
         id: props.selectedItem.id,
       })
       .then(() => {
-        setIsLoading(false)
+        setIsLoading(false);
         props.loadData(
           props.selectedItem.category.name,
-          props.selectedItem.clientType === 'Активные'
-            ? 'active'
-            : props.selectedItem.clientType === 'Потенциальные'
-            ? 'potential'
-            : 'in-progress',
-        )
-        props.setCloseWindow(!props.closeWindow)
-      })
-  }
+          props.selectedItem.clientType === "Активные"
+            ? "active"
+            : props.selectedItem.clientType === "Потенциальные"
+            ? "potential"
+            : "in-progress"
+        );
+        props.setCloseWindow(!props.closeWindow);
+      });
+  };
 
   useEffect(() => {
     if (
@@ -33,21 +33,22 @@ const EditNextContactDate = (props) => {
       props.setShowWindow &&
       props.closeWindow === true
     ) {
-      props.setShowWindow(false)
+      props.setShowWindow(false);
     } else {
-      setDate(props.selectedItem.nextDateContact)
+      setDate(props.selectedItem.nextDateContact);
     }
-  }, [props.selectedItem, props.closeWindow])
+  }, [props.selectedItem, props.closeWindow]);
 
   return (
     <div className="main-form">
+      <div className="main-form__title">Редактирование даты</div>
       <form className="main-form__form">
         <InputDate
           inputName="Дата след. контакта"
           name="nextContactDate"
           selected={Date.parse(date)}
           handleDateChange={(value) => {
-            setDate(value)
+            setDate(value);
           }}
         />
         <div className="main-form__buttons main-form__buttons--full">
@@ -55,7 +56,7 @@ const EditNextContactDate = (props) => {
             className="main-form__submit main-form__submit--inverted"
             type="submit"
             onClick={() => {
-              props.setCloseWindow(!props.closeWindow)
+              props.setCloseWindow(!props.closeWindow);
             }}
             value="Закрыть"
           />
@@ -68,7 +69,7 @@ const EditNextContactDate = (props) => {
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default EditNextContactDate
+export default EditNextContactDate;
