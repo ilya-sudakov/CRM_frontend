49520975d7535d903ede321d0cf43f83f04b9474
ilyasudakov <ilyasudakov@inbox.ru>
2021-02-19 19:52:15 +0300

    add mobile friendly styles

--- src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/ProductionJournal/NotesJournal/NotesJournal.jsx
+++ src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/ProductionJournal/NotesJournal/NotesJournal.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
 import useEmployeesList from "../../../../../../utils/hooks/useEmployeesList.js";
-import { sortByField } from "../../../../../../utils/sorting/sorting.js";
 import SearchBar from "../../../../SearchBar/SearchBar.jsx";
 import { getNotesJournalList } from "../../../../../../utils/RequestsAPI/WorkManaging/notes_journal.js";
 import ControlPanel from "../../../../../../utils/MainWindow/ControlPanel/ControlPanel.jsx";
 import InputDate from "../../../../../../utils/Form/InputDate/InputDate.jsx";
+import TableView from "./TableView.jsx";
 
 import "./NotesJournal.scss";
 
 const NotesJournal = ({}) => {
   const { employees, isLoadingEmployees, workshops } = useEmployeesList();
   const [employeesNotes, setEmployeesNotes] = useState([]);
-  const [isDaysLoaded, setIsDaysLoaded] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [curDay, setCurDay] = useState(new Date());
   const [searchQuery, setSearchQuery] = useState("");
@@ -51,7 +50,6 @@ const NotesJournal = ({}) => {
         });
         break;
     }
-    console.log(newEmployeesNotes);
     return setEmployeesNotes([...newEmployeesNotes]);
   };
 
@@ -68,7 +66,6 @@ const NotesJournal = ({}) => {
       .then(() => getNotesJournalList(prevDay))
       .then(({ data }) => {
         updateEmployeesData(data, "yesterday");
-        setIsDaysLoaded(true);
         setIsLoading(false);
         return;
       })
@@ -102,13 +99,6 @@ const NotesJournal = ({}) => {
     return setEmployeesNotes([...newEmployeesNotes]);
   };
 
-  const filterEmployees = (employees, searchQuery) => {
-    const query = searchQuery.toLowerCase();
-    return employees.filter((employee) =>
-      employee.lastName.toLowerCase().includes(query)
-    );
-  };
-
   return (
     <div className="notes-journal">
       <div className="main-window__header main-window__header--full">
@@ -128,94 +118,14 @@ const NotesJournal = ({}) => {
           />
         }
       />
-      <div className="notes-journal__list">
-        {isLoadingEmployees
-          ? null
-          : workshops.map((workshop) => {
-              const filteredEmployees = sortByField(
-                filterEmployees(employeesNotes, searchQuery).filter(
-                  (employee) => employee.workshop === workshop
-                ),
-                { fieldName: "lastName", direction: "desc" }
-              );
-              if (filteredEmployees.length === 0) return null;
-              return (
-                <div className="notes-journal__list-item">
-                  <span>{workshop}</span>
-                  <div className="notes-journal__employees">
-                    {filteredEmployees.map((employee, index) => {
-                      const prevDay = new Date(
-                        new Date(curDay).setDate(curDay.getDate() - 1)
-                      );
-                      return (
-                        <div
-                          className={`employees__row ${
-                            index % 2 === 0 ? "employees__row--even" : ""
-                          }`}
-                        >
-                          <span>
-                            {`${employee.lastName} ${employee.name} ${employee.middleName}`}
-                            <div>{employee.position}</div>
-                          </span>
-                          <div className="employees__days-wrapper">
-                            <span
-                              className={
-                                prevDay.getDay() === 0 || prevDay.getDay() === 6
-                                  ? "employees__weekend"
-                                  : ""
-                              }
-                            >
-                              <div>Вчера</div>
-                              <textarea
-                                onChange={({ target }) =>
-                                  onInputChange(
-                                    target.value,
-                                    "yesterday",
-                                    employee.id
-                                  )
-                                }
-                                value={employee.workCommentYesterday}
-                                placeholder={
-                                  prevDay.getDay() === 0 ||
-                                  prevDay.getDay() === 6
-                                    ? "Выходной"
-                                    : "Введите текст..."
-                                }
-                              />
-                            </span>
-                            <span
-                              className={
-                                curDay.getDay() === 0 || curDay.getDay() === 6
-                                  ? "employees__weekend"
-                                  : ""
-                              }
-                            >
-                              <div>Сегодня</div>
-                              <textarea
-                                onChange={({ target }) =>
-                                  onInputChange(
-                                    target.value,
-                                    "today",
-                                    employee.id
-                                  )
-                                }
-                                value={employee.workCommentToday}
-                                placeholder={
-                                  curDay.getDay() === 0 || curDay.getDay() === 6
-                                    ? "Выходной"
-                                    : "Введите текст..."
-                                }
-                              />
-                            </span>
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              );
-            })}
-      </div>
+      <TableView
+        isLoadingEmployees={isLoadingEmployees}
+        workshops={workshops}
+        curDay={curDay}
+        employeesNotes={employeesNotes}
+        searchQuery={searchQuery}
+        onInputChange={onInputChange}
+      />
     </div>
   );
 };
---
+++ src/main/resources/src/components/MainPage/GeneralPage/WorkManagement/ProductionJournal/NotesJournal/TableView.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { sortByField } from "../../../../../../utils/sorting/sorting.js";
+
+const TableView = ({
+  isLoadingEmployees,
+  workshops,
+  employeesNotes,
+  searchQuery,
+  curDay,
+  onInputChange,
+}) => {
+  const filterEmployees = (employees, searchQuery) => {
+    const query = searchQuery.toLowerCase();
+    return employees.filter((employee) =>
+      employee.lastName.toLowerCase().includes(query)
+    );
+  };
+
+  return (
+    <div className="notes-journal__list">
+      {isLoadingEmployees
+        ? null
+        : workshops.map((workshop) => {
+            const filteredEmployees = sortByField(
+              filterEmployees(employeesNotes, searchQuery).filter(
+                (employee) => employee.workshop === workshop
+              ),
+              { fieldName: "lastName", direction: "desc" }
+            );
+            if (filteredEmployees.length === 0) return null;
+            return (
+              <div className="notes-journal__list-item">
+                <span>{workshop}</span>
+                <div className="notes-journal__employees">
+                  {filteredEmployees.map((employee, index) => {
+                    const prevDay = new Date(
+                      new Date(curDay).setDate(curDay.getDate() - 1)
+                    );
+                    return (
+                      <div
+                        className={`employees__row ${
+                          index % 2 === 0 ? "employees__row--even" : ""
+                        }`}
+                      >
+                        <span>
+                          {`${employee.lastName} ${employee.name} ${employee.middleName}`}
+                          <div>{employee.position}</div>
+                        </span>
+                        <div className="employees__days-wrapper">
+                          <span
+                            className={
+                              prevDay.getDay() === 0 || prevDay.getDay() === 6
+                                ? "employees__weekend"
+                                : ""
+                            }
+                          >
+                            <div>Вчера</div>
+                            <textarea
+                              onChange={({ target }) =>
+                                onInputChange(
+                                  target.value,
+                                  "yesterday",
+                                  employee.id
+                                )
+                              }
+                              value={employee.workCommentYesterday}
+                              placeholder={
+                                prevDay.getDay() === 0 || prevDay.getDay() === 6
+                                  ? "Выходной"
+                                  : "Введите текст..."
+                              }
+                            />
+                          </span>
+                          <span
+                            className={
+                              curDay.getDay() === 0 || curDay.getDay() === 6
+                                ? "employees__weekend"
+                                : ""
+                            }
+                          >
+                            <div>Сегодня</div>
+                            <textarea
+                              onChange={({ target }) =>
+                                onInputChange(
+                                  target.value,
+                                  "today",
+                                  employee.id
+                                )
+                              }
+                              value={employee.workCommentToday}
+                              placeholder={
+                                curDay.getDay() === 0 || curDay.getDay() === 6
+                                  ? "Выходной"
+                                  : "Введите текст..."
+                              }
+                            />
+                          </span>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+    </div>
+  );
+};
+
+export default TableView;
