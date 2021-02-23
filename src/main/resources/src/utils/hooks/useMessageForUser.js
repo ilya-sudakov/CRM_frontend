dda386e4ac2f613c93b7b4569249fca1bab7c53b
ilyasudakov <ilyasudakov@inbox.ru>
2021-02-23 10:23:28 +0300

    Create useMessageForUser.js

---
+++ src/main/resources/src/utils/hooks/useMessageForUser.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import MessageForUser from "../Form/MessageForUser/MessageForUser.jsx";
+
+const useMessageForUser = (
+  content = {
+    message: "",
+    onClick: () => {},
+    title: "Сообщение",
+    buttonText: "ОК",
+  },
+  updates = []
+) => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {}, [...updates, showMessage]);
+
+  const messageForUser = (
+    <MessageForUser
+      message={content.message}
+      onClick={content.onClick}
+      title={content.title}
+      buttonText={content.buttonText}
+      showMessage={showMessage}
+      setShowMessage={setShowMessage}
+    />
+  );
+
+  return { messageForUser, showMessage, setShowMessage };
+};
+
+export default useMessageForUser;
