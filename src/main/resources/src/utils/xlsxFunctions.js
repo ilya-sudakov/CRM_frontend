4ad4d6025d0b52083e8e0eaa30a000c424d5eb9f
ilyasudakov <ilyasudakov@inbox.ru>
2021-02-28 18:54:41 +0300

    Update xlsxFunctions.js

--- src/main/resources/src/utils/xlsxFunctions.js
+++ src/main/resources/src/utils/xlsxFunctions.js
@@ -5,83 +5,59 @@ import { sortByField } from "./sorting/sorting.js";
 import { getEmployeesByWorkshop } from "./RequestsAPI/Employees.jsx";
 import { getWorkReportByEmployee } from "./RequestsAPI/WorkManaging/WorkControl.jsx";
 
+function s2ab(s) {
+  var buf = new ArrayBuffer(s.length);
+  var view = new Uint8Array(buf);
+  for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xff;
+  return buf;
+}
+
+const clientColWidths = [
+  { width: 30 }, // first column
+  { width: 20 }, // first column
+  { width: 30 }, // first column
+  { width: 40 }, // first column
+];
+
+const saveEmailsCSV = (wb, fileName) => {
+  var wboutput = XLSX2.write(wb, {
+    bookType: "xlsx",
+    bookSST: false,
+    type: "binary",
+  });
+  FileSaver.saveAs(new Blob([s2ab(wboutput)], { type: "" }), fileName);
+};
+
+const addClientContacts = (dataWS, client, index) => {
+  client.contacts.map((contactData) => {
+    const clientRow = [
+      [client.name, client.site, contactData.name, contactData.email],
+    ];
+    if (index === 0) {
+      dataWS = XLSX2.utils.aoa_to_sheet(clientRow);
+    } else {
+      dataWS = XLSX2.utils.sheet_add_aoa(dataWS, clientRow, {
+        origin: "A" + index,
+      });
+    }
+    index++;
+  });
+};
+
 export const exportClientsEmailsCSV = (clients) => {
   let index = 0;
   let dataWS = null;
   Promise.all(
-    sortByField(clients, { fieldName: "name", direction: "asc" }).map(
-      (client) => {
-        client.contacts.map((contactData) => {
-          if (index === 0) {
-            dataWS = XLSX2.utils.aoa_to_sheet([
-              [client.name, client.site, contactData.name, contactData.email],
-            ]);
-          } else {
-            dataWS = XLSX2.utils.sheet_add_aoa(
-              dataWS,
-              [[client.name, client.site, contactData.name, contactData.email]],
-              { origin: "A" + index }
-            );
-          }
-          index++;
-        });
-      }
-    )
+    sortByField(clients, {
+      fieldName: "name",
+      direction: "asc",
+    }).map((client) => addClientContacts(dataWS, client, index))
   ).then(() => {
-    var wscols = [
-      { width: 30 }, // first column
-      { width: 20 }, // first column
-      { width: 30 }, // first column
-      { width: 40 }, // first column
-    ];
-    //Новая ширина столбцов
-    dataWS["!cols"] = wscols;
+    dataWS["!cols"] = clientColWidths; //Новая ширина столбцов
     let wb = XLSX2.utils.book_new(); //Создание новой workbook
     XLSX2.utils.book_append_sheet(wb, dataWS, "Почты");
-    var wboutput = XLSX2.write(wb, {
-      bookType: "xlsx",
-      bookSST: false,
-      type: "binary",
-    });
-    function s2ab(s) {
-      var buf = new ArrayBuffer(s.length);
-      var view = new Uint8Array(buf);
-      for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xff;
-      return buf;
-    }
-    FileSaver.saveAs(
-      new Blob([s2ab(wboutput)], { type: "" }),
-      "ЭлПочты_SendPulse.xlsx"
-    );
+    saveEmailsCSV(wb, "ЭлПочты_SendPulse.xlsx");
   });
-};
-
-/**
- * Select a range of cells
- * @param  {string}   startCell - Range start cell (top-left)
- * @param  {string}   endCell   - Range end cell (bottom-right)
- * @return {Object[]}           - Selected cells
- */
-export const selectCellRange = (sheet, startCell, endCell) => {
-  const [endCellColumn, endRow] = endCell.split(":", 2);
-  const [startCellColumn, startRow] = startCell.split(":", 2);
-
-  let endColumn = sheet.getColumn(endCellColumn);
-  let startColumn = sheet.getColumn(startCellColumn);
-
-  if (!endColumn) throw new Error("End column not found");
-  if (!startColumn) throw new Error("Start column not found");
-
-  const cells = [];
-  for (let y = startRow; y <= endRow; y++) {
-    const row = sheet.getRow(y);
-
-    for (let x = startColumn; x <= endColumn; x++) {
-      cells.push(row.getCell(x));
-    }
-  }
-
-  return cells;
 };
 
 const getReportTableColumnXLSX = (name, width = 5) => {
