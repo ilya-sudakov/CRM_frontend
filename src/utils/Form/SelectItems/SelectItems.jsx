da41abf0eea6e0824633d349e82575edbc7a34b6
ilyasudakov <ilyasudakov@inbox.ru>
2021-03-02 10:05:05 +0300

    update SelectItems

--- src/utils/Form/SelectItems/SelectItems.jsx
+++ src/utils/Form/SelectItems/SelectItems.jsx
@@ -1,60 +1,73 @@
-import React, { useEffect, useState } from 'react'
-import deleteSVG from '../../../../assets/select/delete.svg'
-import './SelectItems.scss'
+import React, { useEffect, useState } from "react";
+import deleteSVG from "../../../../assets/select/delete.svg";
+import AddToButton from "../AddToButton/AddToButton.jsx";
+import "./SelectItems.scss";
 
 const SelectItems = (props) => {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    props.defaultValue && setItems([...props.defaultValue])
-    // console.log(props.defaultValue);
-  }, [props.defaultValue])
+    props.defaultValue && setItems([...props.defaultValue]);
+  }, [props.defaultValue]);
 
-  const handleInputChange = (event) => {
-    let value = event.target.value
-    let name = event.target.name
-    let index = event.target.dataset.id
-    let temp = items
+  const handleInputChange = (index, name, value) => {
+    let temp = items;
     temp.splice(index, 1, {
       ...temp[index],
       [name]: value,
-    })
-    setItems([...temp])
-    props.onChange([...temp])
-  }
+    });
+    setItems([...temp]);
+    props.onChange([...temp]);
+  };
 
   const deleteItem = (event) => {
-    let index = event.target.dataset.id
-    let temp = items
-    temp.splice(index, 1)
-    setItems([...temp])
-    props.onChange([...temp])
-  }
+    let index = event.target.dataset.id;
+    let temp = items;
+    temp.splice(index, 1);
+    setItems([...temp]);
+    props.onChange([...temp]);
+  };
+
+  const getInputElement = (options, index, item) => {
+    return (
+      <div>
+        <span>{options.title}</span>
+        <input
+          type={options.type}
+          name={options.name}
+          onChange={({ target }) =>
+            handleInputChange(index, options.name, target.value)
+          }
+          value={item[options.name]}
+          readOnly={props.readOnly}
+          placeholder={options.placeholder}
+          autoComplete="off"
+        />
+      </div>
+    );
+  };
 
   return (
     <div className="select-items">
       <div className="select-items__input">
         <div className="select-items__input_name">
-          {props.inputName + (props.required ? '*' : '')}
+          {props.inputName + (props.required ? "*" : "")}
+          {!props.readOnly && (
+            <AddToButton
+              text="Добавить элемент"
+              onClick={() => {
+                let temp = items;
+                temp.push({
+                  name: "",
+                  quantity: "",
+                });
+                setItems([...temp]);
+                props.onChange([...temp]);
+              }}
+            />
+          )}
         </div>
         <div className="select-items__input-field">
-          {!props.readOnly && (
-            <button
-              onClick={(event) => {
-                event.preventDefault()
-                let temp = items
-                temp.push({
-                  name: '',
-                  quantity: '',
-                })
-                setItems([...temp])
-                props.onChange([...temp])
-              }}
-              className="select-items__button"
-            >
-              Добавить элемент
-            </button>
-          )}
           <div className="select-items__list">
             {props.error === true && (
               <div
@@ -75,32 +88,26 @@ const SelectItems = (props) => {
             {items.map((item, index) => {
               return (
                 <div className="select-items__list-item">
-                  <div>
-                    <span>Название</span>
-                    <input
-                      readOnly={props.readOnly}
-                      type="text"
-                      name="name"
-                      data-id={index}
-                      onChange={handleInputChange}
-                      value={item.name}
-                      placeholder="Введите название..."
-                      autoComplete="off"
-                    />
-                  </div>
-                  <div>
-                    <span>Кол-во</span>
-                    <input
-                      readOnly={props.readOnly}
-                      type="number"
-                      name="quantity"
-                      data-id={index}
-                      onChange={handleInputChange}
-                      value={item.quantity}
-                      placeholder="Введите кол-во..."
-                      autoComplete="off"
-                    />
-                  </div>
+                  {getInputElement(
+                    {
+                      title: "Название",
+                      placeholder: "Введите название...",
+                      type: "text",
+                      name: "name",
+                    },
+                    index,
+                    item
+                  )}
+                  {getInputElement(
+                    {
+                      title: "Кол-во",
+                      placeholder: "Введите кол-во...",
+                      type: "number",
+                      name: "quantity",
+                    },
+                    index,
+                    item
+                  )}
                   {!props.readOnly && (
                     <img
                       src={deleteSVG}
@@ -110,15 +117,14 @@ const SelectItems = (props) => {
                       title="Удалить элемент"
                     />
                   )}
-                  {/* {items.length > 1 && <img src={deleteSVG} className="select-items__img" onClick={deleteItem} data-id={index} title="Удалить элемент" />} */}
                 </div>
-              )
+              );
             })}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SelectItems
+export default SelectItems;
