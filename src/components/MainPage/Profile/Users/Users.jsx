6f24cbc97b5b65993fe651d80a0c6839da82b519
ilyasudakov <ilyasudakov@inbox.ru>
2021-03-03 13:47:06 +0300

    Update Users.jsx

--- src/components/MainPage/Profile/Users/Users.jsx
+++ src/components/MainPage/Profile/Users/Users.jsx
@@ -1,119 +1,119 @@
-import React, { useEffect, useState } from 'react'
-import './Users.scss'
-import '../../../../utils/MainWindow/MainWindow.scss'
-import SearchBar from '../../SearchBar/SearchBar.jsx'
-import TableView from './TableView/TableView.jsx'
-import { getUsers, deleteUser } from '../../../../utils/RequestsAPI/Users.jsx'
-import FloatingPlus from '../../../../utils/MainWindow/FloatingPlus/FloatingPlus.jsx'
-import ControlPanel from '../../../../utils/MainWindow/ControlPanel/ControlPanel.jsx'
+import React, { useEffect, useState } from "react";
+import "./Users.scss";
+import "../../../../utils/MainWindow/MainWindow.scss";
+import SearchBar from "../../SearchBar/SearchBar.jsx";
+import TableView from "./TableView/TableView.jsx";
+import { getUsers, deleteUser } from "../../../../utils/RequestsAPI/Users.jsx";
+import FloatingPlus from "../../../../utils/MainWindow/FloatingPlus/FloatingPlus.jsx";
+import ControlPanel from "../../../../utils/MainWindow/ControlPanel/ControlPanel.jsx";
 
 const Users = (props) => {
-  const [searchQuery, setSearchQuery] = useState('')
-  const [users, setUsers] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [searchQuery, setSearchQuery] = useState("");
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [userRoles, setUserRoles] = useState([
     {
-      name: 'Руководитель',
-      role: 'ROLE_ADMIN',
+      name: "Руководитель",
+      role: "ROLE_ADMIN",
       visible: false,
     },
     {
-      name: 'Инженер',
-      role: 'ROLE_ENGINEER',
+      name: "Инженер",
+      role: "ROLE_ENGINEER",
       visible: false,
     },
     {
-      name: 'Менеджер1',
-      role: 'ROLE_MANAGER',
+      name: "Менеджер1",
+      role: "ROLE_MANAGER",
       visible: false,
     },
     {
-      name: 'ЦехЛЭМЗ',
-      role: 'ROLE_LEMZ',
+      name: "ЦехЛЭМЗ",
+      role: "ROLE_LEMZ",
       visible: false,
     },
     {
-      name: 'ЦехЛепсари',
-      role: 'ROLE_LEPSARI',
+      name: "ЦехЛепсари",
+      role: "ROLE_LEPSARI",
       visible: false,
     },
     {
-      name: 'ЦехЛиговский',
-      role: 'ROLE_LIGOVSKIY',
+      name: "ЦехЛиговский",
+      role: "ROLE_LIGOVSKIY",
       visible: false,
     },
     {
-      name: 'Диспетчер',
-      role: 'ROLE_DISPATCHER',
+      name: "Диспетчер",
+      role: "ROLE_DISPATCHER",
       visible: false,
     },
-  ])
+  ]);
 
   useEffect(() => {
-    document.title = 'Управление пользователями'
-    let abortController = new AbortController()
-    loadUsers(abortController.signal)
+    document.title = "Управление пользователями";
+    let abortController = new AbortController();
+    loadUsers(abortController.signal);
     return function cancel() {
-      abortController.abort()
-    }
-  }, [])
+      abortController.abort();
+    };
+  }, []);
 
   const deleteItem = (id) => {
-    deleteUser(id).then(() => loadUsers())
-  }
+    deleteUser(id).then(() => loadUsers());
+  };
 
   const loadUsers = (signal) => {
-    setIsLoading(true)
+    setIsLoading(true);
     getUsers(signal)
       .then((res) => res.json())
       .then((response) => {
-        setUsers(response)
-        setIsLoading(false)
+        setUsers(response);
+        setIsLoading(false);
       })
       .catch((error) => {
-        console.log(error)
-        setIsLoading(false)
-      })
-  }
+        console.log(error);
+        setIsLoading(false);
+      });
+  };
 
   // * Sorting
 
   const [sortOrder, setSortOrder] = useState({
-    curSort: 'username',
-    username: 'asc',
-  })
+    curSort: "username",
+    username: "asc",
+  });
 
   const changeSortOrder = (event) => {
-    const name = event.target.value.split(' ')[0]
-    const order = event.target.value.split(' ')[1]
+    const name = event.target.value.split(" ")[0];
+    const order = event.target.value.split(" ")[1];
     setSortOrder({
       curSort: name,
       [name]: order,
-    })
-  }
+    });
+  };
 
   const filterSearchQuery = (data) => {
     return data.filter((item) =>
-      item.username.toLowerCase().includes(searchQuery.toLowerCase()),
-    )
-  }
+      item.username.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  };
 
   const sortUsers = (data) => {
     return filterSearchQuery(data).sort((a, b) => {
       if (a[sortOrder.curSort] < b[sortOrder.curSort]) {
-        return sortOrder[sortOrder.curSort] === 'desc' ? 1 : -1
+        return sortOrder[sortOrder.curSort] === "desc" ? 1 : -1;
       }
       if (a[sortOrder.curSort] > b[sortOrder.curSort]) {
-        return sortOrder[sortOrder.curSort] === 'desc' ? -1 : 1
+        return sortOrder[sortOrder.curSort] === "desc" ? -1 : 1;
       }
-      return 0
-    })
-  }
+      return 0;
+    });
+  };
 
   return (
     <div className="users-manage">
       <div className="main-window">
-        <FloatingPlus linkTo="/profile/users/new" visibility={['ROLE_ADMIN']} />
+        <FloatingPlus linkTo="/profile/users/new" visibility={["ROLE_ADMIN"]} />
         <div className="main-window__header main-window__header--full">
           <div className="main-window__title">Управление пользователями</div>
         </div>
@@ -142,9 +142,9 @@ const Users = (props) => {
                     <div
                       className={
                         (role.visible
-                          ? 'main-window__button'
-                          : 'main-window__button main-window__button--inverted') +
-                        ' main-window__list-item--' +
+                          ? "main-window__button"
+                          : "main-window__button main-window__button--inverted") +
+                        " main-window__list-item--" +
                         role.className
                       }
                       onClick={() => {
@@ -152,18 +152,18 @@ const Users = (props) => {
                           return {
                             ...role,
                             visible: false,
-                          }
-                        })
+                          };
+                        });
                         temp.splice(index, 1, {
                           ...role,
                           visible: !role.visible,
-                        })
-                        setUserRoles([...temp])
+                        });
+                        setUserRoles([...temp]);
                       }}
                     >
                       {role.name}
                     </div>
-                  )
+                  );
                 })}
               </div>
             </div>
@@ -171,25 +171,25 @@ const Users = (props) => {
         />
         <TableView
           data={sortUsers(users).filter((item) => {
-            let check = false
-            let noActiveRoles = true
+            let check = false;
+            let noActiveRoles = true;
             userRoles.map((role) => {
               userRoles.map((role) => {
                 if (role.visible) {
-                  noActiveRoles = false
+                  noActiveRoles = false;
                 }
-              })
+              });
               if (
                 noActiveRoles === true ||
                 (role.visible &&
                   item.roles.find((roleTemp) => roleTemp.name === role.role) !==
                     undefined)
               ) {
-                check = true
-                return
+                check = true;
+                return;
               }
-            })
-            return check
+            });
+            return check;
           })}
           searchQuery={searchQuery}
           deleteItem={deleteItem}
@@ -199,7 +199,7 @@ const Users = (props) => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
