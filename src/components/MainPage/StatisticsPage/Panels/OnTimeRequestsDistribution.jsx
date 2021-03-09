98ae0faa4d178149e61277a76623b108e977b27f
ilyasudakov <ilyasudakov@inbox.ru>
2021-03-09 10:55:39 +0300

    Update OnTimeRequestsDistribution.jsx

--- src/components/MainPage/StatisticsPage/Panels/OnTimeRequestsDistribution.jsx
+++ src/components/MainPage/StatisticsPage/Panels/OnTimeRequestsDistribution.jsx
@@ -23,45 +23,10 @@ const OnTimeRequestsDistribution = ({
     [currDate],
   );
 
-  const getStats = (requests) => {
-    setStats((stats) => ({
-      ...stats,
-      isLoading: true,
-      isLoaded: false,
-    }));
-
-    let curMonthOnTimeQuantity = 0;
-    let curMonthAllQuantity = 0;
-    let prevMonthOnTimeQuantity = 0;
-    let prevMonthAllQuantity = 0;
-
-    //check prev month
-    let temp = requests.filter((request) => {
-      const date = new Date(request.date);
-      const prevMonth = getPrevData(currDate.startDate);
-      if (
-        checkIfDateIsInRange(date, prevMonth.startDate, prevMonth.endDate) &&
-        (request.status === 'Завершено' || request.status === 'Отгружено')
-      ) {
-        //если заказ отгружен вовремя
-        if (
-          Math.abs(
-            dateDiffInDays(
-              new Date(request.date),
-              new Date(request.shippingDate),
-            ),
-          ) <= 7
-        ) {
-          prevMonthOnTimeQuantity++;
-        }
-        prevMonthAllQuantity++;
-        return false;
-      }
-      return true;
-    });
-
-    //check cur month
-    const filteredRequests = temp.filter((request) => {
+  const filterRequests = (requests, currDate) => {
+    let monthOnTime = 0;
+    let monthAllQuantity = 0;
+    requests.filter((request) => {
       const date = new Date(request.date);
       if (
         checkIfDateIsInRange(date, currDate.startDate, currDate.endDate) &&
@@ -76,13 +41,37 @@ const OnTimeRequestsDistribution = ({
             ),
           ) <= 7
         ) {
-          curMonthOnTimeQuantity++;
+          monthOnTime++;
         }
-        curMonthAllQuantity++;
-        return true;
+        monthAllQuantity++;
+        return false;
       }
-      return false;
+      return true;
     });
+    return [requests, monthOnTime, monthAllQuantity];
+  };
+
+  const getStats = (requests) => {
+    setStats((stats) => ({
+      ...stats,
+      isLoading: true,
+      isLoaded: false,
+    }));
+
+    //check prev month
+    const prevMonth = getPrevData(currDate.startDate);
+    const [
+      temp,
+      prevMonthOnTimeQuantity,
+      prevMonthAllQuantity,
+    ] = filterRequests(requests, prevMonth);
+
+    //check cur month
+    const [
+      filteredRequests,
+      curMonthOnTimeQuantity,
+      curMonthAllQuantity,
+    ] = filterRequests(temp, currDate);
 
     //соотношение вовремя выпол. заказов в тек. месяце
     const curMonthValue =
