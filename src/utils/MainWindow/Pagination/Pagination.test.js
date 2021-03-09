2f6e116160c11dfaa5c3c273d3e59d58ba1586b4
ilyasudakov <ilyasudakov@inbox.ru>
2021-03-09 15:12:53 +0300

    Update Pagination.test.js

--- src/utils/MainWindow/Pagination/Pagination.test.js
+++ src/utils/MainWindow/Pagination/Pagination.test.js
@@ -36,4 +36,61 @@ describe('Pagination component', () => {
     fireEvent.click(prevPageButton);
     // expect(await screen.findByText("14")).toBeInTheDocument();
   });
+
+  it('user clicks on prev page', async () => {
+    renderWithRouter(
+      <Pagination itemsCount={300} curPage={15} setCurPage={(item) => item} />,
+    );
+    const prevPageButton = document.getElementsByClassName(
+      'main-window__page-number--skip',
+    )[0];
+    fireEvent.click(prevPageButton);
+    // expect(await screen.findByText("14")).toBeInTheDocument();
+  });
+
+  it('user clicks on in between page', async () => {
+    renderWithRouter(
+      <Pagination itemsCount={300} curPage={15} setCurPage={(item) => item} />,
+    );
+    const inBetweenPageButton = document.getElementsByClassName(
+      'main-window__page-number',
+    )[2];
+    fireEvent.click(inBetweenPageButton);
+    // expect(await screen.findByText("14")).toBeInTheDocument();
+  });
+
+  it('user clicks on last page', async () => {
+    renderWithRouter(
+      <Pagination itemsCount={600} curPage={15} setCurPage={(item) => item} />,
+    );
+    let lastPageButton = document.getElementsByClassName(
+      'main-window__page-number',
+    );
+    lastPageButton = lastPageButton[lastPageButton.length - 2];
+    fireEvent.click(lastPageButton);
+    // expect(await screen.findByText("14")).toBeInTheDocument();
+  });
+
+  it('user clicks on first page', async () => {
+    renderWithRouter(
+      <Pagination itemsCount={300} curPage={15} setCurPage={(item) => item} />,
+    );
+    let lastPageButton = document.getElementsByClassName(
+      'main-window__page-number',
+    );
+    lastPageButton = lastPageButton[1];
+    fireEvent.click(lastPageButton);
+    // expect(await screen.findByText("14")).toBeInTheDocument();
+  });
+
+  it('user clicks on next page', async () => {
+    renderWithRouter(
+      <Pagination itemsCount={600} curPage={15} setCurPage={(item) => item} />,
+    );
+    const nextPageButton = document.getElementsByClassName(
+      'main-window__page-number--skip',
+    )[1];
+    fireEvent.click(nextPageButton);
+    // expect(await screen.findByText("14")).toBeInTheDocument();
+  });
 });
