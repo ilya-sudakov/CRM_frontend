74071ddbecdb244fed1dae3b93f078b1a3e97be5
ilyasudakov <ilyasudakov@inbox.ru>
2021-03-19 21:38:42 +0300

    move some ImageView functions to separate file

--- src/utils/Form/ImageView/ImageView.jsx
+++ src/utils/Form/ImageView/ImageView.jsx
@@ -1,74 +1,14 @@
 import { createRef, useEffect } from 'react';
 import useFormWindow from 'Utils/hooks/useFormWindow';
 import Button from 'Utils/Form/Button/Button.jsx';
-import { imgToBlobDownload } from 'Utils/functions.jsx';
-import axios from 'axios';
 
 import './ImageView.scss';
-
-const getFileExtension = (item) => {
-  const isBase64 =
-    (typeof item === 'string' && item.length > 1000) ||
-    (typeof item === 'string' && item.length === 0);
-  const isLocalPath = typeof item === 'string' && item.length <= 200;
-  const isRemoteFile =
-    typeof item === 'object' && item?.url !== undefined && item?.url !== null;
-  if (isRemoteFile)
-    return item?.url?.split(/\/fileWithoutDB\/downloadFile\/[^.]*./)[1];
-  if (isLocalPath) return item.split('.')[1];
-  if (isBase64) return item.split('image/')[1]?.split(';base64')[0];
-  return item?.type?.split('/')[1] ?? 'file';
-};
-
-const getFileName = (item) => {
-  const isBase64 =
-    (typeof item === 'string' && item.length > 1000) ||
-    (typeof item === 'string' && item.length === 0);
-  const isLocalPath = typeof item === 'string' && item.length <= 200;
-  const isRemoteFile =
-    typeof item === 'object' && item?.url !== undefined && item?.url !== null;
-  const isRawFile =
-    typeof item === 'object' &&
-    item?.name !== undefined &&
-    item?.name !== null &&
-    item?.size !== undefined &&
-    item?.size !== null;
-  if (isRemoteFile)
-    return item?.url?.split(/\/fileWithoutDB\/downloadFile\//)[1];
-  if (isLocalPath) return item.split('assets/')[1];
-  if (isBase64) return 'Просмотр';
-  if (isRawFile) return item?.name;
-  return item?.type?.split('/')[1] ?? 'file';
-};
-
-const downloadImage = (_file) => {
-  const fileName = getFileName(_file);
-  const isLocalPath = typeof _file === 'string' && _file.length <= 200;
-  if (_file?.url || isLocalPath) {
-    return axios
-      .get(_file?.url ?? _file, {
-        responseType: 'blob',
-      })
-      .then(({ data }) => {
-        let url = window.URL.createObjectURL(new Blob([data]));
-        let a = document.createElement('a');
-        a.href = url;
-        a.download = fileName;
-        a.click();
-      });
-  }
-  return imgToBlobDownload(_file, fileName);
-};
-
-const getLink = (_file) => {
-  const isLocalPath = typeof _file === 'string' && _file.length <= 200;
-  if (_file?.url || isLocalPath) return _file?.url ?? _file;
-};
-
-const copyLinkToClipboard = (_file) => {
-  const link = getLink(_file);
-  navigator.clipboard.writeText(link);
-};
+import {
+  getFileName,
+  downloadImage,
+  getFileExtension,
+  copyLinkToClipboard,
+} from './functions';
 
 const ImageView = ({ file, closeWindow = null }) => {
   const imgRef = createRef(null);
---
+++ src/utils/Form/ImageView/functions.js
@@ -0,0 +1,66 @@
+import { imgToBlobDownload } from 'Utils/functions.jsx';
+import axios from 'axios';
+
+export const getFileExtension = (item) => {
+  const isBase64 =
+    (typeof item === 'string' && item.length > 1000) ||
+    (typeof item === 'string' && item.length === 0);
+  const isLocalPath = typeof item === 'string' && item.length <= 200;
+  const isRemoteFile =
+    typeof item === 'object' && item?.url !== undefined && item?.url !== null;
+  if (isRemoteFile)
+    return item?.url?.split(/\/fileWithoutDB\/downloadFile\/[^.]*./)[1];
+  if (isLocalPath) return item.split('.')[1];
+  if (isBase64) return item.split('image/')[1]?.split(';base64')[0];
+  return item?.type?.split('/')[1] ?? 'file';
+};
+
+export const getFileName = (item) => {
+  const isBase64 =
+    (typeof item === 'string' && item.length > 1000) ||
+    (typeof item === 'string' && item.length === 0);
+  const isLocalPath = typeof item === 'string' && item.length <= 200;
+  const isRemoteFile =
+    typeof item === 'object' && item?.url !== undefined && item?.url !== null;
+  const isRawFile =
+    typeof item === 'object' &&
+    item?.name !== undefined &&
+    item?.name !== null &&
+    item?.size !== undefined &&
+    item?.size !== null;
+  if (isRemoteFile)
+    return item?.url?.split(/\/fileWithoutDB\/downloadFile\//)[1];
+  if (isLocalPath) return item.split('assets/')[1];
+  if (isBase64) return 'Просмотр';
+  if (isRawFile) return item?.name;
+  return item?.type?.split('/')[1] ?? 'file';
+};
+
+export const downloadImage = (_file) => {
+  const fileName = getFileName(_file);
+  const isLocalPath = typeof _file === 'string' && _file.length <= 200;
+  if (_file?.url || isLocalPath) {
+    return axios
+      .get(_file?.url ?? _file, {
+        responseType: 'blob',
+      })
+      .then(({ data }) => {
+        let url = window.URL.createObjectURL(new Blob([data]));
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        a.click();
+      });
+  }
+  return imgToBlobDownload(_file, fileName);
+};
+
+export const getLink = (_file) => {
+  const isLocalPath = typeof _file === 'string' && _file.length <= 200;
+  if (_file?.url || isLocalPath) return _file?.url ?? _file;
+};
+
+export const copyLinkToClipboard = (_file) => {
+  const link = getLink(_file);
+  navigator.clipboard.writeText(link);
+};
