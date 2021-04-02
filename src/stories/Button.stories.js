bf1f8ca111716ad79ff28cfe0a61f0c96b90666b
ilyasudakov <ilyasudakov@inbox.ru>
2021-04-02 16:55:41 +0300

    add storybook integration

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,32 @@
+import Button from '../utils/Form/Button/Button.jsx';
+
+export default {
+  title: 'UI elements/Button',
+  component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  text: 'Текст кнопки',
+};
+
+export const Inverted = Template.bind({});
+Inverted.args = {
+  text: 'Текст кнопки',
+  inverted: true,
+};
+
+export const DefaultLoading = Template.bind({});
+DefaultLoading.args = {
+  text: 'Текст кнопки',
+  isLoading: true,
+};
+
+export const InvertedLoading = Template.bind({});
+InvertedLoading.args = {
+  text: 'Текст кнопки',
+  inverted: true,
+  isLoading: true,
+};
