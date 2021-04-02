0646c141f70dc9ce11bbd7e91ee1f459a64563e4
ilyasudakov <ilyasudakov@inbox.ru>
2021-04-02 19:13:52 +0300

    add deploy storybook to ghpages work

--- src/stories/Searchbar.stories.js
+++ src/stories/Searchbar.stories.js
@@ -9,3 +9,40 @@ const Template = (args) => <SearchBar {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const WithOptions = Template.bind({});
+WithOptions.args = {
+  searchOptions: [
+    {
+      text: 'Везде',
+      value: '',
+    },
+    {
+      text: 'Город',
+      value: 'city',
+    },
+  ],
+};
+
+const advanced = [
+  {
+    type: 'groupCheckbox',
+    parentCheckbox: { text: 'Поиск по налогообложению', value: false },
+    childCheckbox: { text: 'УСН', value: false },
+  },
+];
+
+export const WithAdvancedOptions = Template.bind({});
+WithAdvancedOptions.args = {
+  searchOptions: [
+    {
+      text: 'Везде',
+      value: '',
+    },
+    {
+      text: 'Город',
+      value: 'city',
+    },
+  ],
+  advancedOptions: advanced,
+};
