78deaa92fa3662aa0b25b9e5f422d2c7d0a48e47
ilyasudakov <ilyasudakov@inbox.ru>
2021-04-13 17:28:30 +0300

    Update ControlPanel.jsx

--- src/utils/MainWindow/ControlPanel/ControlPanel.jsx
+++ src/utils/MainWindow/ControlPanel/ControlPanel.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import ChevronSVG from 'Assets/tableview/chevron-down.inline.svg';
 import './ControlPanel.scss';
 
-const ControlPanel = (props) => {
+const ControlPanel = ({
+  styles,
+  sorting,
+  content,
+  panelName,
+  buttons,
+  itemsCount,
+}) => {
   const [isHidden, setIsHidden] = useState(true);
 
   return (
-    <div className="control-panel" style={{ ...(props.styles ?? {}) }}>
+    <div className="control-panel" style={{ ...(styles ?? {}) }}>
       <div className="main-window__control-panel-wrapper">
         <div className="control-panel__buttons">
-          {props.sorting || null}
-          {props.content ? (
+          {sorting || null}
+          {content ? (
             <div
               className="main-window__button main-window__button--inverted main-window__button--filter"
               onClick={() => setIsHidden((isHidden) => !isHidden)}
             >
-              <span>{props.panelName || 'Фильтры'}</span>
+              <span>{panelName || 'Фильтры'}</span>
               <ChevronSVG
                 className={`main-window__img ${
                   isHidden ? '' : 'main-window__img--rotated'
@@ -23,15 +31,24 @@ const ControlPanel = (props) => {
               />
             </div>
           ) : null}
-          {props.buttons || null}
-          {props.itemsCount ? (
-            <div className="main-window__amount_table">{props.itemsCount}</div>
+          {buttons || null}
+          {itemsCount ? (
+            <div className="main-window__amount_table">{itemsCount}</div>
           ) : null}
         </div>
-        {!isHidden ? props.content : null}
+        {!isHidden ? content : null}
       </div>
     </div>
   );
 };
 
 export default ControlPanel;
+
+ControlPanel.propTypes = {
+  styles: PropTypes.object,
+  sorting: PropTypes.node,
+  content: PropTypes.node,
+  panelName: PropTypes.string,
+  buttons: PropTypes.any,
+  itemsCount: PropTypes.string,
+};
